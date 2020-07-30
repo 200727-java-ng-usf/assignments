11a7ff29721f8b70b8523ba81ddc26f12db5fd4d
capolimia <capolimia@gmail.com>
2020-07-29 22:50:46 -0400

    Adding quesrion 2, Updated assignment driver for testing q two, and assignment gitignore

--- mia_capoli_code/Java/untitled1/src/main/java/com/revature/Assignment1Driver.java
+++ mia_capoli_code/Java/untitled1/src/main/java/com/revature/Assignment1Driver.java
@@ -1,10 +1,13 @@
 package com.revature;
 
 import com.revature.questions.Question1;
+import com.revature.questions.Question2;
 
 public class Assignment1Driver {
     public static void main(String[] args) {
-        Question1 questionOne = new Question1();
-        questionOne.bubbleSort();
+//        Question1 questionOne = new Question1();
+//        questionOne.bubbleSort();
+        Question2 questionTwo = new Question2();
+        questionTwo.fibonacciSequence();
     }
 }
--- mia_capoli_code/Java/untitled1/src/main/java/com/revature/questions/Question2.java
+++ mia_capoli_code/Java/untitled1/src/main/java/com/revature/questions/Question2.java
@@ -1,4 +1,30 @@
 package com.revature.questions;
 
 public class Question2 {
+
+    private void printValues(int[] arr) {
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+
+    public void fibonacciSequence() {
+        int[] fibonacciArray = new int[25];
+        for (int i = 0; i < 25; i++) {
+            if (i == 0) {
+                fibonacciArray[0] = 0;
+            } else if (i == 1) {
+                fibonacciArray[1] = 1;
+            } else if (i == 2){
+                fibonacciArray[2] = 1;
+            } else {
+                fibonacciArray[i] = fibonacciArray[i-1] + fibonacciArray[i-2];
+            }
+        }
+
+        printValues(fibonacciArray);
+
+    }
 }
