4e3cd7f0676bd790d21266c58604a0f4501412cd
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-01 14:19:06 -0700

    Began ReverseString

--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/ReverseString.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/ReverseString.java
@@ -1,4 +1,36 @@
 package mainpackage;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ReverseString {
+    public ReverseString() {
+
+        // Begin with a string, convert to arraylist
+        String string = "Revature Java";
+        List<Character> chars = new <>();
+
+        for(char ch: string.toCharArray()) {
+            chars.add(ch);
+        }
+
+        System.out.println(chars);
+
+        public static<T> List<T> reverseList(List<T> list) {
+            List<T> reverse = new ArrayList<>(list.size());
+            list.stream()
+                    .collect(Collectors.toCollection(LinkedList::new))
+                    .descendingIterator()
+                    .forEachRemaining(reverse::add);
+
+            return reverse;
+        }
+
+        List<> reverse = reverseList(list);
+        System.out.println(reverse);
+
+    }
 }
