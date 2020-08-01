f40c972fb9a44d4f72cfa6db0e72196a5575f267
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-01 15:08:14 -0700

    Begin SwitchDemo

--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/SwitchDemo.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/SwitchDemo.java
@@ -1,4 +1,28 @@
 package mainpackage;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class SwitchDemo {
+
+
+    // Need to have the datetime available
+    DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+    LocalDateTime now = LocalDateTime.now();
+
+    // And have the string
+    String string = "I am learning Core Java";
+
+    // Build the cases in the switcher
+    public SwitchDemo() {
+        switch (expression) {
+            sqrtcase: System.out.println(Math.sqrt(double a));
+            break;
+            datecase: System.out.println(dtf.format(now));
+            break;
+            splitcase: System.out.println(string[] = string.split(" "));
+            break;
+
+        }
+    }
 }
