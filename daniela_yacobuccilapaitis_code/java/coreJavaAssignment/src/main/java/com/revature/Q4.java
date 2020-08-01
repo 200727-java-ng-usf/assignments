ce4884e8ee3cdacbc7f976ce8e32987be538f82a
Daniela Yacobucci-Lapaitis <dyacobucci47@gmail.com>
2020-07-31 20:17:21 -0400

    moved coreJavaAssignment to assignments folder

---
+++ daniela_yacobuccilapaitis_code/java/coreJavaAssignment/src/main/java/com/revature/Q4.java
@@ -0,0 +1,26 @@
+package com.revature;
+
+import java.util.Scanner;
+
+//  Q4. Write a program to compute N factorial.
+public class Q4 {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a number ");
+        int num = (int)scan.next();
+        System.out.println();
+    }
+
+    public static int calcFactorial(int num) {
+        if (num == 0||num == 1)
+            return 1;
+        else
+        return (num * calcFactorial(num-1) * calcFactorial(num-2));
+    }
+
+
+
+
+}
