e9d53a6a0eb5f8b318e8ac75e60eb0f824c34c57
SeanRog <seanmikerog@gmail.com>
2020-07-31 23:36:22 -0700

    Finished Question 20

---
+++ sean_rogers_code/Java/src/main/java/Question20.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Question20 {
+    /*
+    Mickey:Mouse:35:Arizona
+    Hulk:Hogan:50:Virginia
+    Roger:Rabbit:22:California
+    Wonder:Woman:18:Montana
+
+    Write a program that would read from the file and print it out to the screen in the following
+    format:
+
+    Name: Mickey Mouse
+    Age: 35 years
+    State: Arizona State
+     */
+    public StringBuilder fileOutput() {
+        File file = new File("textfile.txt");
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        String[] words;
+        try{
+            FileReader fileReader = new FileReader(file);
+            Scanner scanner = new Scanner(fileReader);
+            while(scanner.hasNext()) {
+                line = scanner.nextLine();
+                words = line.split(":");
+                stringBuilder.append("Name: " + words[0] + " " + words[1] + "\n" + "Age: " + words[2] + " years\n" + "State: " + words[3] + " State\n\n");
+            }
+            System.out.println(stringBuilder.toString());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return stringBuilder;
+    }
+}
