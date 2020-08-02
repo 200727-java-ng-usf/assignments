5a49ecda116b12df78daa7ba5fcac67ab8f9bec1
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-02 08:40:16 -0700

    Fixed AppDriver

--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/AppDriver.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/AppDriver.java
@@ -4,100 +4,106 @@ import java.util.Arrays;
 
 public class AppDriver {
     public static void main(String[] args) {
+        System.out.println("1. Bubble Sort an array");
+        System.out.println("Array Before Bubble Sort");
         BubbleSort bubbleSort = new BubbleSort();
-//        BubbleSort bubbleSort() {
-//            System.out.println("1. Bubble Sort an array");
-//            System.out.println("Array Before Bubble Sort");
-//            for(int i=0; i < arr.length; i++){
-//                System.out.print(arr[i] + " ");
-//            }
-//            System.out.println();
-//
-//            bubbleSort(arr);//sorting array elements using bubble sort
-//
-//            System.out.println("Array After Bubble Sort");
-//            for(int i=0; i < arr.length; i++){
-//                System.out.print(arr[i] + " ");
-//            }
-//            System.out.println("+------------------------+");
-//        }
+
+        System.out.println("Array After Bubble Sort");
+
+        System.out.println("+------------------------+");
+
+        System.out.println("2. Generate Fibonacci series");
         Fibonacci fibonacci = new Fibonacci();
         System.out.println(Arrays.toString(fibonacci.getFibonacciArray()));
-//        Fibonacci fibonacci(){
-//            System.out.println("2. Generate Fibonacci series");
-//
-//            System.out.println("+------------------------+");
-//        }
-//        ReverseString reverseString() {
-//            System.out.println("3. Reverse a string without using .reverse");
-//            System.out.println("+------------------------+");
-//        }
-//        Nfactorial nfactorial() {
-//            System.out.println("4. Calculate N-factorial");
-//            System.out.println("+------------------------+");
-//        }
-//        SubStringerMethod subStringerMethod() {
-//            System.out.println("5. Return a substring within ints");
-//            System.out.println("+------------------------+");
-//        }
-//        IntEven intEven() {
-//            System.out.println("6. Determine if int is even without using modulo");
-//            System.out.println("+------------------------+");
-//        }
-//        EmployeeSort employeeSort() {
-//            System.out.println("7. Sort employees by name, department, and age");
-//            System.out.println("+------------------------+");
-//        }
-//        Palindromes palindromes() {
-//            System.out.println("8. Extract palindromes from an array list");
-//            System.out.println("+------------------------+");
-//        }
-//        PrimeNumbers primeNumbers() {
-//            System.out.println("9. Print out prime numbers");
-//            System.out.println("+------------------------+");
-//        }
-//        FindMinimum findMinimum() {
-//            System.out.println("10. Find the minimum of two numbers");
-//            System.out.println("+------------------------+");
-//        }
-//        PackageFloat packageFloat() {
-//            System.out.println("11. Access another package");
-//            System.out.println("+------------------------+");
-//        }
-//        Evens evens() {
-//            System.out.println("12. Print even numbers");
-//            System.out.println("+------------------------+");
-//        }
-//        Triangle triangle() {
-//            System.out.println("13. Generate triangle using loop");
-//            System.out.println("+------------------------+");
-//        }
-//        SwitchDemo switchDemo() {
-//            System.out.println("14. Demonstrate the switch case");
-//            System.out.println("+------------------------+");
-//        }
-//        ArithmeticImpl arithmetic() {
-//            System.out.println("15. Implement an interface");
-//            System.out.println("+------------------------+");
-//        }
-//        NumberOfCharacters numberOfCharacters() {
-//            System.out.println("16. Count number of characters in a string");
-//            System.out.println("+------------------------+");
-//        }
-//        InterestCalculator interestCalculator() {
-//            System.out.println("17. Calculate principal, rate, and time");
-//            System.out.println("+------------------------+");
-//        }
-//        ChangeCase changeCase() {
-//            System.out.println("18. Change the case of characters");
-//            System.out.println("+------------------------+");
-//        }
-//        AddUpArrayList addUpArrayList() {
-//            System.out.println("19. From array list, add up evens and add up odds, remove prime numbers");
-//            System.out.println("+------------------------+");
-//        }
-//        NotePadReader notePadReader() {
-//            System.out.println("20. Read from NotePad");
-//        }
+        System.out.println("+------------------------+");
+
+        System.out.println("3. Reverse a string without using .reverse");
+        ReverseString reverseString = new ReverseString();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("4. Calculate N-factorial");
+        Nfactorial nfactorial = new Nfactorial();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("5. Return a substring within ints");
+        SubStringerMethod subStringerMethod = new SubStringerMethod();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("6. Determine if int is even without using modulo");
+        IntEven intEven = new IntEven();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("7. Sort employees by name, department, and age");
+        EmployeeSort employeeSort = new EmployeeSort();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("8. Extract palindromes from an array list");
+        Palindromes palindromes = new Palindromes();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("9. Print out prime numbers");
+        PrimeNumbers primeNumbers = new PrimeNumbers();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("10. Find the minimum of two numbers");
+        FindMinimum findMinimum = new FindMinimum();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("11. Access another package");
+        PackageFloat packageFloat = new PackageFloat();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("12. Print even numbers");
+        Evens evens = new Evens();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("13. Generate triangle using loop");
+        Triangle triangle = new Triangle();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("14. Demonstrate the switch case");
+        SwitchDemo switchDemo = new SwitchDemo();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("15. Implement an interface");
+        ArithmeticImpl arithmetic = new ArithmeticImpl();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("16. Count number of characters in a string");
+        NumberOfCharacters numberOfCharacters = new NumberOfCharacters();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("17. Calculate principal, rate, and time");
+        InterestCalculator interestCalculator = new InterestCalculator();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("18. Change the case of characters");
+        ChangeCase changeCase = new ChangeCase();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("19. From array list, add up evens and add up odds, remove prime numbers");
+        AddUpArrayList addUpArrayList = new AddUpArrayList();
+
+        System.out.println("+------------------------+");
+
+        System.out.println("20. Read from NotePad");
+        NotePadReader notePadReader = new NotePadReader();
+
     }
 }
