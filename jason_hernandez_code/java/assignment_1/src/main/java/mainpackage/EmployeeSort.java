1ded04f0db6a2258f9fa1acdd5bc86a16715d08e
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-02 10:44:56 -0700

    Began SubStringerMethod and EmployeeSort

--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/EmployeeSort.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/EmployeeSort.java
@@ -1,4 +1,58 @@
 package mainpackage;
 
-public class EmployeeSort {
+import java.util.Comparator;
+
+public class EmployeeSort implements Comparator {
+
+    // First create the params
+    private String name;
+    private String department;
+    private int age;
+    void Employee(){
+
+    }
+
+    // Plug the params into the variable
+    void Employee(String n, String d, int a) {
+        name = n;
+        department = d;
+        age = a;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    // The sort is done here
+    public void compare(Employee n1, Employee n2) {
+
+    }
+    public void compare(Employee d1, Employee d2) {
+
+    }
+    public void compare(Employee a1, Employee a2) {
+
+    }
+
 }
--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/SubStringerMethod.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/SubStringerMethod.java
@@ -1,4 +1,31 @@
 package mainpackage;
 
 public class SubStringerMethod {
+
+    public SubStringerMethod() {
+
+        // Begin with a string
+        String str = "Substringing a string";
+    }
 }
+
+// Example of using the forbidden substring methods, from tutorials point
+// public class StringDemo {
+//
+//    public static void main(String[] args) {
+//
+//        String str = "This is tutorials point";
+//        String substr = "";
+//
+//        // prints the substring after index 7 till index 17
+//        substr = str.substring(7, 17);
+//        System.out.println("substring = " + substr);
+//
+//        // prints the substring after index 0 till index 7
+//        substr = str.substring(0, 7);
+//        System.out.println("substring = " + substr);
+//    }
+//}
+//      Outputs:
+//        substring =  tutorials
+//        substring = This is
