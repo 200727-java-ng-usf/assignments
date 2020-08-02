ed665c6083f5f61c36e14ea476733e63dacc3c73
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-02 14:29:58 -0700

    Did InterestCalculator

--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/InterestCalculator.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/InterestCalculator.java
@@ -1,4 +1,27 @@
 package mainpackage;
 
+import java.util.Scanner;  // Import the Scanner class
+
 public class InterestCalculator {
-}
+    public InterestCalculator() {
+
+        // Begin by taking input
+        Scanner principal = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter principal ");
+        float amtPrincipal = principal.nextFloat();  // Read user input
+
+        Scanner rate = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter interest rate ");
+        float interestRate = rate.nextFloat();  // Read user input
+
+        Scanner howLong = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter the amount of time ");
+        float amtTime = howLong.nextFloat();  // Read user input
+
+        // Calculate the interest
+        float interest = amtPrincipal * interestRate * amtTime;
+        System.out.println("Interest earned this period is: " + interest);  // Output calculated amount
+    }
+
+    }
+
--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/PackageFloat.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/PackageFloat.java
@@ -5,6 +5,6 @@ import secondpackage.*;
 public class PackageFloat {
     public void StoreFloat() {
         System.out.println("The value of pi is " + a);
-        System.out.println(" The value of e is " + b);
+        System.out.println("The value of e is " + b);
     }
 }
