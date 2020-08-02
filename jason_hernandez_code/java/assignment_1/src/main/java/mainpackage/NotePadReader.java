ab4382346e65278b20bf2f050cad6478f22db48d
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-02 14:53:14 -0700

    Began NotePadReader

--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/NotePadReader.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/NotePadReader.java
@@ -1,4 +1,15 @@
 package mainpackage;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class NotePadReader {
+    private NotePadReader() {
+        NotePadReader parser = new NotePadReader
+                ("C:\\Users\\jason\\200727-java-ng-usf\\assignments\\jason_hernandez_code\\java\\assignment_1\\src\\main\\resources");
+        parser.processLineByLine();
+
+    }
 }
