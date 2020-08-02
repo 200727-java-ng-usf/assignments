b7400aae353a4947ad4c8d9f255c82ef9c24138b
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-02 13:51:21 -0700

    PrimeNumbers and Evens share similar code

--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/Evens.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/Evens.java
@@ -1,4 +1,36 @@
 package mainpackage;
 
+import java.util.ArrayList;
+
 public class Evens {
+    public Evens() {
+        // Initialize two arrays
+        ArrayList<Integer> numbers2 = new ArrayList<Integer>(100);
+        ArrayList<Integer> evenNumbers = new ArrayList<Integer>(100);
+
+        // Populate the first array with integers 1 to 100
+        for (int i = 1; i <= 100; i++)
+            numbers2.add(i);
+
+        // Check each number to see if it is even
+        int temp;
+        boolean isEven=true;
+
+        for(int j=2; j<=numbers2/2; j++)
+        {
+            temp = numbers2%2;
+            if(temp==0)
+            {
+                isEven=false;
+                break;
+            }
+            //If isEven is true then the number is even else not
+            if(isEven)
+                evenNumbers.add(j);
+
+        }
+        System.out.println("The Even Numbers are " + evenNumbers);
+
+    }
+    }
 }
--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/PrimeNumbers.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/PrimeNumbers.java
@@ -1,4 +1,36 @@
 package mainpackage;
 
+import java.util.ArrayList;
+
 public class PrimeNumbers {
+    public PrimeNumbers() {
+
+        // Initialize two arrays
+        ArrayList<Integer> numbers = new ArrayList<Integer>(100);
+        ArrayList<Integer> primes = new ArrayList<Integer>(100);
+
+        // Populate the first array with integers 1 to 100
+        for (int i = 1; i <= 100; i++)
+            numbers.add(i);
+
+        // Check each number to see if it is prime
+        int temp;
+        boolean isPrime=true;
+
+        for(int j=2; j<=numbers/2; j++)
+        {
+            temp = numbers%j;
+            if(temp==0)
+            {
+                isPrime=false;
+                break;
+            }
+            //If isPrime is true then the number is prime else not
+            if(isPrime)
+                primes.add(j);
+
+        }
+        System.out.println("The Prime Numbers are " + primes);
+
+    }
 }
