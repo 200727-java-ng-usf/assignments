78f937a311165e671c6e1f4ba5104457c5ca5bb1
mcoffma1 <47868221+mcoffma1@users.noreply.github.com>
2020-08-03 10:18:56 -0400

    Added Q5, Q6, and Q7

---
+++ michael_coffman_code/Homework1/src/main/java/com/revature/Question5.java
@@ -0,0 +1,49 @@
+package com.revature;
+
+// Importing Scanner
+import java.util.Scanner;
+
+public class Question5 {
+
+    private static void answer() {
+
+        // Instantiating the input object for the String and Int
+        Scanner input = new Scanner(System.in);
+
+        // Instantiating variables from user input
+        System.out.println("Enter a String.");
+        String str = input.nextLine();
+        char[] temp = str.toCharArray();
+        String result = "";
+
+        System.out.println("+-------------------+");
+
+        System.out.println("Enter an integer.");
+        int idx = input.nextInt();
+
+        // Closing the Scanner object
+        input.close();
+
+        System.out.println("+-------------------+");
+
+        // for loop to take char[] indices and add them to the empty String result
+        for (int i = 0; i < idx; i++) {
+            result += temp[i];
+        }
+
+        System.out.println(result);
+
+    }
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        answer();
+
+    }
+}
