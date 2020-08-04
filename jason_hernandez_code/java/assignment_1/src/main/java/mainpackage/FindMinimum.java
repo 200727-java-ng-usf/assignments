050ed9a86691ee18d1090532999cc0695f136bbd
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-04 13:58:10 -0700

    Code for FindMinimum

--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/FindMinimum.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/FindMinimum.java
@@ -1,4 +1,12 @@
 package mainpackage;
 
 public class FindMinimum {
+    public FindMinimum() {
+        int num1 = 6, num2 = 9, result;
+        System.out.println("The numbers are: " + num1 + " and " + num2 );
+
+        result = num1 < num2 ? num1:num2;
+
+        System.out.println("Smallest Number is:"+result);
+    }
 }
