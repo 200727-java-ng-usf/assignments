f6265ad65534465d6af00d4b2812e137e5ced405
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-08-04 18:02:46 -0400

    working on Question1, finished Question12

---
+++ troy_davis_code/java/assignment1/question1/src/main/java/com/revature/question1/Question1.java
@@ -0,0 +1,56 @@
+/*
+    Q1. Perform a bubble sort on the following integer array:  1,0,5,6,3,2,3,7,9,8,4
+ */
+
+package com.revature.question1;
+
+import java.util.Arrays;
+
+public class Question1 {
+    private int[] givenArray;
+    private int[] sortedArray;
+
+    public Question1(int[] givenArray){
+        this.givenArray = givenArray;
+        this.sortedArray = sortThisArray(this.givenArray);
+    }
+
+    private int[] sortThisArray(int[] anArray){
+        int[] tempArray = anArray;
+        for(int i = tempArray.length-1; i > 0; i--){
+            for (int j = 0; j < 1; j++){
+                if (tempArray[j] > tempArray[j+1]) {
+                    int n = tempArray[j];
+                    tempArray[j] = tempArray[j + 1];
+                    tempArray[j + 1] = n;
+                }
+            }
+        }
+
+        return tempArray;
+    }
+
+    public int[] getGivenArray() {
+        return givenArray;
+    }
+
+    public void setGivenArray(int[] givenArray) {
+        this.givenArray = givenArray;
+    }
+
+    public int[] getSortedArray() {
+        return sortedArray;
+    }
+
+    public void setSortedArray(int[] sortedArray) {
+        this.sortedArray = sortedArray;
+    }
+
+    @Override
+    public String toString() {
+        return "Question1{" + "\n"+
+                "givenArray=" + Arrays.toString(givenArray) + "\n"+
+                "sortedArray=" + Arrays.toString(sortedArray) + "\n"+
+                '}';
+    }
+}
---
+++ troy_davis_code/java/assignment1/question1/src/test/java/com/revature/qeustion1Test/Question1Test.java
@@ -0,0 +1,21 @@
+/*
+    Q1. Perform a bubble sort on the following integer array:  1,0,5,6,3,2,3,7,9,8,4
+ */
+package com.revature.qeustion1Test;
+
+import com.revature.question1.Question1;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Question1Test {
+    int[] test = {1,0,5,6,3,2,7,9,8,4};
+    Question1 q = new Question1(test);
+
+    @Test
+    public void notNullTest(){
+        //test to make sure that the fields are not null
+        System.out.println(q.toString());
+        Assert.assertTrue(q.getGivenArray() != null);
+        Assert.assertTrue(q.getSortedArray() != null);
+    }
+}
