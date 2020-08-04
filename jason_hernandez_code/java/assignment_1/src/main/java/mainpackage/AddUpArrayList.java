ac5cdc52a5089a94a3b8354e1ef6ea406048385d
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-03 21:30:01 -0700

    Code for AddUpArrayList

--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/AddUpArrayList.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/AddUpArrayList.java
@@ -1,4 +1,48 @@
 package mainpackage;
+import java.util.ArrayList;
 
 public class AddUpArrayList {
-}
+    public AddUpArrayList() {
+
+        // Initialize three arrayLists
+        ArrayList<Integer> numbers3 = new ArrayList<Integer>(100);
+        ArrayList<Integer> evenNumbers2 = new ArrayList<Integer>(50);
+        ArrayList<Integer> oddNumbers = new ArrayList<Integer>(50);
+
+        // Populate the first array with integers 1 to 100
+        for (int i = 1; i <= 100; i++)
+            numbers3.add(i);
+
+        // Check each number to see if it is even
+        int temp;
+        boolean isEven = true;
+
+        for (int j = 2; j <= numbers3 / 2; j++) {
+            temp = numbers3 % 2;
+            if (temp == 0) {
+                isEven = false;
+                break;
+            }
+            //If isEven is true then the number is even else not
+            if (isEven)
+                evenNumbers2.add(j);
+            else
+                oddNumbers.add(j);
+
+            // Add up the even numbers
+            double sumEven = 0;
+            for (int k : evenNumbers2) {
+                sumEven += k;
+            }
+
+            // Add up the odd numbers
+            double sumOdd = 0;
+            for (int h : evenNumbers2) {
+                sumOdd += h;
+            }
+            System.out.println("Sum of Even numbers: " + sumEven);
+            System.out.println("Sum of odd numbers: " + sumOdd);
+
+        }
+
+    }}
--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/ArithmeticImpl.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/ArithmeticImpl.java
@@ -1,24 +1,71 @@
 package mainpackage;
 
+import java.util.Scanner;
+
 public class ArithmeticImpl implements Arithmetic {
 
-    @Override
-    public void addUp() {
 
+        Scanner firstNum = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter first number ");
+        float firstNumber = firstNum.nextFloat();  // Read user input
+
+        Scanner secNum = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Enter first number ");
+        float secondNumber = secNum.nextFloat();  // Read user input
+
+        @Override
+        public void addUp() {
+            float sumUp = firstNumber + secondNumber;
+            System.out.println("Sum: " + sumUp);
+        }
+
+        @Override
+        public void subtract () {
+            float diff = firstNumber - secondNumber;
+            System.out.println("Difference: " + diff);
+        }
+
+        @Override
+        public void multiply () {
+            float mult = firstNumber * secondNumber;
+            System.out.println("Product: " + mult);
+        }
+
+        @Override
+        public void divideBy () {
+            float quotient = firstNumber / secondNumber;
+            System.out.println("Quotient: " + quotient);
+        }
+
+    public Scanner getFirstNum() {
+        return firstNum;
     }
 
-    @Override
-    public void subtract() {
-
+    public void setFirstNum(Scanner firstNum) {
+        this.firstNum = firstNum;
     }
 
-    @Override
-    public void multiply() {
-
+    public float getFirstNumber() {
+        return firstNumber;
     }
 
-    @Override
-    public void divideBy() {
+    public void setFirstNumber(float firstNumber) {
+        this.firstNumber = firstNumber;
+    }
 
+    public Scanner getSecNum() {
+        return secNum;
+    }
+
+    public void setSecNum(Scanner secNum) {
+        this.secNum = secNum;
+    }
+
+    public float getSecondNumber() {
+        return secondNumber;
+    }
+
+    public void setSecondNumber(float secondNumber) {
+        this.secondNumber = secondNumber;
     }
 }
