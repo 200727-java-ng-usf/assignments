eb86ea4d528bd5cd44c521b3a8355fb08c7ef367
SeanRog <seanmikerog@gmail.com>
2020-08-03 21:20:11 -0700

    Created JUnit TestDriver.java, Assignment1TestSuite.java, and Assignment1Test.java. Moved all sysout tests to Assignment1Test.java and replaced sysout tests with JUnit assert methods

---
+++ sean_rogers_code/Java/src/test/java/Assignment1TestSuite.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        Assignment1Test.class
+})
+
+public class Assignment1TestSuite {
+
+
+}
