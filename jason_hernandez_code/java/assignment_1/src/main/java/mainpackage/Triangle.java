2138ba7fd5cb5ff3dec018cf29aa6a5711a40f2e
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-05 13:52:26 -0700

    Did Triangle

--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/Triangle.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/Triangle.java
@@ -1,10 +1,29 @@
 package mainpackage;
 
-public class Triangle {
-    private String binaries = "0101010101";
+public class Triangle<rows> {
+    public void Triangle() {
+        Triangle triangle = new Triangle();
+        // Adapted from codeforcoding.com/java-program-to-display-triangle-binary-pattern/
 
+        // Declare fields
+        int i, j, rows;
 
+        // Define number of rows
+        rows=4;
+
+        // Iterate over the rows
+        for(i=1;i<=rows;i++) {
+            // Alternate 0 and 1
+            for (j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 1) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+
+            }
+            // Print the triangle in one go
+            System.out.println();
+        }
+    }
 }
-
-//  General pattern suggested by teammate
-//  The pattern is a series of 10101.... wrapped into a pyramid shape
