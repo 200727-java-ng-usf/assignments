952793062d69afef8bf978c08a34f2b17b3158e5
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-08-05 18:05:59 -0400

    finished for the day

---
+++ troy_davis_code/java/assignment1/question8/src/main/java/com/revature/question8/Question8.java
@@ -0,0 +1,25 @@
+/*
+    Q8. Write a program that stores the following strings in an ArrayList and saves all the palindromes in another ArrayList.
+
+“karan”, “madam”, ”tom”, “civic”, “radar”, “jimmy”, “kayak”, “john”,  “refer”, “billy”, “did”
+ */
+package com.revature.question8;
+
+import java.util.ArrayList;
+
+public class Question8 {
+    private ArrayList<String> storageList;
+    private ArrayList<String> palindromeList;
+
+    public Question8(String... someStrings){
+
+    }
+
+
+
+    private boolean isThisAPalindrome(String someString){
+        boolean bool = false;
+
+        return bool;
+    }
+}
---
+++ troy_davis_code/java/assignment1/question8/src/test/java/com/revature/question8Test/Question8Test.java
@@ -0,0 +1,7 @@
+package com.revature.question8Test;
+
+import com.revature.question8.Question8;
+
+public class Question8Test {
+    //Question8 q = new Question8();
+}
