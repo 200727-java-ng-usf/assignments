ca3b094a67df6cff199a367cc9992cc3355a501a
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-08-07 13:55:45 -0500

    moved bubble sort algorithm to method instead of main

--- farren_springer_code/java/AssignmentOne/src/main/java/QuestionOne.java
+++ farren_springer_code/java/AssignmentOne/src/main/java/QuestionOne.java
@@ -2,35 +2,39 @@ import java.util.Arrays;
 
 public class QuestionOne {
 
-    public static void main(String[] args) {
-        // Establish the string to be used from the worksheet
-        int someInts[] = {1, 0, 5, 6, 3, 2, 3, 7, 9, 8, 4};
+    public static int[] bubbleSort(int[] arr) {
 
         // Create a temporary int variable to store for comparisons
         int temp;
+
+        // Use loops to compare the integers in the array
+        for (int i = arr.length - 1; i > 0; i--) {
+            // For each index value, compare it to each other value in the array
+            for (int j = 0; j < i; j++) {
+                // If the current array index value is greater
+                if (arr[j] > arr[j + 1]) {
+                    // Then store that in a temporary location
+                    temp = arr[j];
+                    // Move the compared value to the location it was compared to
+                    arr[j] = arr[j + 1];
+                    // Assign the temp variable that held the greater value to its rightful place
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        // Establish the string to be used from the worksheet
+        int someInts[] = {1, 0, 5, 6, 3, 2, 3, 7, 9, 8, 4};
 
         // Print the unsorted array
         System.out.println("This is the unsorted array: " + Arrays.toString(someInts) + "\n");
         System.out.print("This is the bubble sorted array: ");
 
-        // Use loops to compare the integers in the array
-        for (int i = someInts.length - 1; i > 0; i--) {
-            // For each index value, compare it to each other value in the array
-            for (int j = 0; j < i; j++) {
-                // If the current array index value is greater
-                if (someInts[j] > someInts[j + 1]) {
-                    // Then store that in a temporary location
-                    temp = someInts[j];
-                    // Move the compared value to the location it was compared to
-                    someInts[j] = someInts[j + 1];
-                    // Assign the temp variable that held the greater value to its rightful place
-                    someInts[j + 1] = temp;
-                }
-            }
-        }
-
         // Print the bubble sorted array
-        System.out.println(Arrays.toString(someInts));
+        System.out.println(Arrays.toString(bubbleSort(someInts)));
 
     } // end main
 } // end class
---
+++ farren_springer_code/java/AssignmentOne/src/test/java/QuestionOneTests.java
@@ -0,0 +1,18 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class QuestionOneTests {
+
+    /**
+     * methods to test in QuestionOne
+     *
+     */
+
+    private QuestionOne sut; // SUT = System Under Test
+
+
+
+}
