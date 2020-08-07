e90d185194a4fbaae221442cd0bd4c58acb1fdee
Shea Wilson <sheawilsn@gmail.com>
2020-08-07 14:07:25 -0500

    Question 11 done i think

---
+++ shea_wilson_code/maven/CoreJavaAssignment/src/main/java/com/revature/QuestionEleven.java
@@ -0,0 +1,19 @@
+package com.revature;
+
+import com.revature.numbers.ForQuestionEleven;
+
+
+public class QuestionEleven {
+
+
+    public static void main(String[] args) {
+
+        System.out.println(new ForQuestionEleven());
+        //System.out.println();
+
+
+    }
+
+
+
+}
---
+++ shea_wilson_code/maven/CoreJavaAssignment/src/main/java/com/revature/numbers/ForQuestionEleven.java
@@ -0,0 +1,16 @@
+package com.revature.numbers;
+
+public class ForQuestionEleven {
+
+        public ForQuestionEleven() {
+
+                float val2 = 10.05f;
+
+                float val1 = 50.12f;
+
+                System.out.println(val2+"\n"+val1);
+
+        }
+}
+
+
