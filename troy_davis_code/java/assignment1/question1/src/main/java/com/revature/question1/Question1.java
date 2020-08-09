1c8b8b5429b575907945044665c9e2962a327b09
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-08-08 21:48:40 -0400

    Finished question 1!

--- troy_davis_code/java/assignment1/question1/src/main/java/com/revature/question1/Question1.java
+++ troy_davis_code/java/assignment1/question1/src/main/java/com/revature/question1/Question1.java
@@ -2,41 +2,59 @@
     Q1. Perform a bubble sort on the following integer array:
         - 1,0,5,6,3,2,3,7,9,8,4
  */
-
 package com.revature.question1;
-
 import java.util.Arrays;
 
 public class Question1 {
+    //create class fields for the given and sorted arrays
     private int[] givenArray;
     private int[] sortedArray;
 
     public Question1(int[] givenArray){
-        this.givenArray = givenArray;
+        //make a copy of the initial array
+        this.givenArray = Arrays.copyOf(givenArray, givenArray.length);
+        //auto-sort the array on instantiation of the question object
         this.sortedArray = sortThisArray(this.givenArray);
     }
 
     private int[] sortThisArray(int[] anArray){
-        int[] tempArray = anArray;
+        //check for null value
+        if (anArray == null){
+            return new int[]{};
+        } else if(anArray.length <= 1){
+            //check for an array of less than 1 in length(no need to sort)
+            return anArray;
+        }
+        int[] tempArray = Arrays.copyOf(anArray, anArray.length);
+        //iterate each number through the array
         for(int i = tempArray.length-1; i > 0; i--){
-            for (int j = 0; j < 1; j++){
+            //bubble each number through the array
+            for (int j = 0; j < i; j++){
+                //compare this number to the next number
                 if (tempArray[j] > tempArray[j+1]) {
+                    //initialize  new temp. number set to current number
                     int n = tempArray[j];
+                    //the current number = the next number
                     tempArray[j] = tempArray[j + 1];
+                    //the next number equals the current number
                     tempArray[j + 1] = n;
                 }
             }
         }
-
+        //return the array
         return tempArray;
     }
 
+    //auto-generated getters and setters:
     public int[] getGivenArray() {
         return givenArray;
     }
 
     public void setGivenArray(int[] givenArray) {
+        //object can be passed new arrays to be sorted after instantiation
+        //these arrays will be auto sorted
         this.givenArray = givenArray;
+        this.sortedArray = sortThisArray(this.givenArray);
     }
 
     public int[] getSortedArray() {
@@ -53,5 +71,21 @@ public class Question1 {
                 "givenArray=" + Arrays.toString(givenArray) + "\n"+
                 "sortedArray=" + Arrays.toString(sortedArray) + "\n"+
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Question1)) return false;
+        Question1 question1 = (Question1) o;
+        return Arrays.equals(getGivenArray(), question1.getGivenArray()) &&
+                Arrays.equals(getSortedArray(), question1.getSortedArray());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Arrays.hashCode(getGivenArray());
+        result = 31 * result + Arrays.hashCode(getSortedArray());
+        return result;
     }
 }
--- troy_davis_code/java/assignment1/question1/src/test/java/com/revature/qeustion1Test/Question1Test.java
+++ troy_davis_code/java/assignment1/question1/src/test/java/com/revature/qeustion1Test/Question1Test.java
@@ -5,18 +5,51 @@
 package com.revature.qeustion1Test;
 
 import com.revature.question1.Question1;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class Question1Test {
-    int[] test = {1,0,5,6,3,2,7,9,8,4};
-    Question1 q = new Question1(test);
+    int[] test;
+    int[] proof;
+    Question1 sut;
+
+    @Before
+    public void setup(){
+        //set the test conditions for each test
+        test = new int[]{1, 0, 5, 6, 3, 2, 7, 9, 8, 4};
+        proof = new int[]{0,1,2,3,4,5,6,7,8,9};
+        sut = new Question1(test);
+    }
+
+    @After
+    public void tearDown(){
+        //reset the system under test
+        sut = null;
+    }
 
     @Test
     public void notNullTest(){
-        //test to make sure that the fields are not null
-        System.out.println(q.toString());
-        Assert.assertTrue(q.getGivenArray() != null);
-        Assert.assertTrue(q.getSortedArray() != null);
+        String msg = "test to make sure that the fields are not null";
+        System.out.println(sut.toString());
+        Assert.assertTrue(sut.getGivenArray() != null);
+        Assert.assertTrue(sut.getSortedArray() != null);
+    }
+    @Test
+    public void proofTest(){
+        String msg = "does the method return an array that matches a proof array?";
+        Assert.assertArrayEquals(msg,proof, sut.getSortedArray());
+    }
+    @Test
+    public void testWithNull(){
+        String msg = "If given a null array, the class should return an empty array";
+        //generate null array
+        int[] nullArray = null;
+        //ser given array to the null value
+        sut.setGivenArray(nullArray);
+        //generate empty array
+        int[] expectedResult = {};
+        Assert.assertArrayEquals(msg,expectedResult, sut.getSortedArray());
     }
 }
