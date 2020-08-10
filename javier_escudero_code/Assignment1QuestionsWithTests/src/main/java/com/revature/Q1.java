f67d0943abe02c2a08fe81865959b6f29eea7dea
escuderj <javierusn@yahoo.com>
2020-08-09 20:20:52 -0400

    finished Q1 and Q1Test

--- javier_escudero_code/Assignment1QuestionsWithTests/src/main/java/com/revature/Q1.java
+++ javier_escudero_code/Assignment1QuestionsWithTests/src/main/java/com/revature/Q1.java
@@ -1,7 +1,34 @@
 package com.revature;
-
+import java.util.Arrays;
 
 public class Q1 {
-    System.out.println("nothing yet");
+    /**    Q1. Perform a bubble sort on the following integer array:  1,0,5,6,3,2,3,7,9,8,4
+     *
+     * @param arr is the array with integer values to be passed from Q1Test.java and tested in Q1.java
+     * @return the sorted integer array, otherwise might return an empty array
+     */
+    public int[] sort(int[] arr) {
 
+        // check for the edge cases
+        if (arr == null || arr.length == 0) {
+            return new int[0];
+        }
+
+        // make a copy of the original array, this copy will be sorted instead of the original
+        int[] copy = Arrays.copyOf(arr, arr.length);
+        // sort the array
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                //if current number on the left is bigger than on the right, swap
+                if (copy[j] > copy[j + 1]) {
+                    // swap the values
+                    int temp = copy[j];
+                    copy[j] = copy[j + 1];
+                    copy[j + 1] = temp;
+                }
+            }
+        }
+        return copy;
+    }
 }
--- javier_escudero_code/Assignment1QuestionsWithTests/src/test/java/com/revature/Q1Test.java
+++ javier_escudero_code/Assignment1QuestionsWithTests/src/test/java/com/revature/Q1Test.java
@@ -1,7 +1,34 @@
 package com.revature;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.revature.Q1;
+import org.junit.Before;
+import org.junit.Test;
 
-class Q1Test {
+import static org.junit.Assert.*;
+/*
+    Tests for the bubble sort method in Q1
+ */
+public class Q1Test {
 
+    private Q1 sut; // SUT = System Under Test
+
+    @Before
+    public void t1Setup(){
+        sut = new Q1();
+    }
+
+    @Test
+    public void testSolutionWithGivenArray() {
+        String msg = "If a randomly sorted array is provided, the impl should return the array sorted.";
+        int[] testArray = {1,0,5,6,3,2,3,7,9,8,4};
+        int[] expectedResult = {0,1,2,3,3,4,5,6,7,8,9};
+        assertArrayEquals(msg, expectedResult, sut.sort(testArray));
+    }
+
+    @Test
+    public void testSolutionWithNullArray() {
+        String msg = "If a null array is provided, them impl should return an empty array.";
+        int[] expectedResult = {};
+       assertNotNull(sut.sort(expectedResult));
+    }
 }
