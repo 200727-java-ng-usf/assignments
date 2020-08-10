1bafae21a75128d671539df4d76a949746ac7791
Shea Wilson <sheawilsn@gmail.com>
2020-08-10 14:20:10 -0500

    added comments to Q17

--- shea_wilson_code/maven/CoreJavaAssignment/src/main/java/com/revature/QuestionSeventeen.java
+++ shea_wilson_code/maven/CoreJavaAssignment/src/main/java/com/revature/QuestionSeventeen.java
@@ -4,26 +4,30 @@ import java.util.Scanner;
 
 public class QuestionSeventeen {
 
-    private static double simpleInterest(long p, float r , int t){
+    private static double simpleInterest(long p, float r , int t){ //method to return the the interest rate
 
+        // initializing fields with parameters of the method
         int time = t;
         float rate = r;
         long prin = p;
 
-        double interest = prin * rate * time ;
+        double interest = prin * rate * time ; // initialize interest with product
 
         return interest;
     }
 
     public static void main(String[] args) {
         Scanner user = new Scanner(System.in);
+        // Request user input
         System.out.print("Please enter the principle: ");
+        // Accept user input
         long userLong = user.nextLong();
         System.out.print("Please enter the rate (Please use decimal value to represent percentage): ");
         float userFloat = user.nextFloat();
         System.out.print("Please the time in years: ");
         int userInt = user.nextInt();
 
+        // method call to find the product of user input
         double totalInterest = simpleInterest(userLong,userFloat,userInt);
         System.out.println("+---------------------------------+");
         System.out.printf("Thank you for your input! \n"
