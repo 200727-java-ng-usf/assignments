d4d2beaac31d8378f60a4c7e49deceb4701a5def
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-08-10 15:26:25 -0500

    q13 documentation done

--- farren_springer_code/java/AssignmentOne/src/main/java/QuestionThirteen.java
+++ farren_springer_code/java/AssignmentOne/src/main/java/QuestionThirteen.java
@@ -1,25 +1,50 @@
 public class QuestionThirteen {
 
+    /**
+     * This class has a method called that wraps a string and displays
+     * substrings separated by line.
+     * @param theStringToBeWrapped
+     * @return
+     */
     public static String wrapTheString (String theStringToBeWrapped) {
 
+        /**
+         * Initialize the result to empty.
+         */
         String result = "";
 
-        // For 4 lines,
+        /**
+         * The string will be split into four lines.
+         */
         for (int i = 0; i < 4; i++) {
-            // make a substring that holds an increasing amount of characters
+            /**
+             * Make a substring that holds an increasing amount of characters
+              */
             result += theStringToBeWrapped.substring(0, i*2) + "\n";
         }
+        /**
+         * Return the result.
+         */
         return result;
     }
 
+    /**
+     * The main method demonstrates the use of the wrapTheString method.
+     * @param args
+     */
     public static void main(String[] args) {
 
-        // The string from the question
+        /**
+         * Declare and initialize the string specified in the question.
+         */
         String onesAndZeros = "0 1 0 1 0 1 0 1 0 1 ";
 
-        // Call the method
+        /**
+         * This will demonstrate the use of the wrapTheString method to the console.
+         */
         System.out.println(wrapTheString(onesAndZeros));
 
     } // end main
+
 } // end class
 // done
