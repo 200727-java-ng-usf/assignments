af6cb0bf79f7237e506fd6465764a68046465534
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-08-10 15:35:30 -0500

    q12 documentation done

--- farren_springer_code/java/AssignmentOne/src/main/java/QuestionTwelve.java
+++ farren_springer_code/java/AssignmentOne/src/main/java/QuestionTwelve.java
@@ -1,43 +1,68 @@
 import java.util.Arrays;
 
+/**
+ * This class has a method, arrayOperations, that prints all even numbers in an array of integers and
+ * a main method that demonstrates the use of arrayOperations.
+ */
 public class QuestionTwelve {
 
-    // Method that takes in a number that is the length of the array
-    public static int[] arrayOperations (int length){
+    /**
+     * The arrayOperations method has an integer parameter to specify the length of the array and
+     * returns the even numbers.
+     * @param length
+     * @return
+     */
+    public static void arrayOperations (int length){
 
-        // Create an int array with length specified
+        /**
+         * This declares an integer array of a length specified by the method implementation.
+         */
         int[] theIntArray = new int[length];
 
-        // store 1 through 100 in the array
-        for(int i = 1; i < 101; i++) {
+        /**
+         * Parse through the array and store consecutive values.
+         */
+        for(int i = 1; i < theIntArray.length + 1; i++) {
             theIntArray[i-1] = i;
         }
 
+        /**
+         * Print the original array to the console.
+         */
         System.out.println("Here is the 100 array filled with numbers 1 through 100: " + Arrays.toString(theIntArray)); // test
 
-        // print the even numbers
+        /**
+         * Print the even numbers to the console.
+         */
         System.out.print("Here is a list of the even numbers in the array: ");
         for(int i : theIntArray) {
-            // iterate through numbers 2 through 50; divide the number
-            // in every index of hundred array to check if it
-            // can be factored by that number
+            /**
+             * If the integer at each index divided by two has a zero remainder, print it.
+             */
             if(theIntArray[i-1]%2 == 0){
 
                 System.out.print(theIntArray[i-1]);
 
             }
             else {
+                /**
+                 * This puts space where there would be an odd number.
+                 */
                 System.out.print(" ");
             }
         }
 
-        // Return the array
-        return theIntArray;
-    }
+    } // end arrayOperations
 
-    // Main method
+    /**
+     * The main method will demonstrate the use of the arrayOperations method.
+     * @param args
+     */
     public static void main(String[] args) {
 
+        /**
+         * Pass 100 as the integer parameter of the method.
+         */
         arrayOperations(100);
 
     } // end main
