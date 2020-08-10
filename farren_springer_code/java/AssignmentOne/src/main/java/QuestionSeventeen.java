bb84bce0e7eb5ab5d79c3cd3d656487062f92b3c
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-08-10 15:08:50 -0500

    q17 documentation done

--- farren_springer_code/java/AssignmentOne/src/main/java/QuestionSeventeen.java
+++ farren_springer_code/java/AssignmentOne/src/main/java/QuestionSeventeen.java
@@ -1,51 +1,84 @@
 import java.util.Scanner;
 
-public class QuestionSeventeen {
-    /*
-    Write a program that calculates the simple interest on the principal, rate of interest and
-    number of years provided by the user. Enter principal, rate and time through the console
-    using the Scanner class.
-        Interest = Principal* Rate* Time
-     */
+/**
+ * This class has a method that calculates simple interest and a main method to
+ * demonstrate that method.
+ */
 
+public class QuestionSeventeen {
+
+    /**
+     * This method takes in three values: a principal, a rate, and a time, and returns the
+     * simple interest.
+     * @param principal
+     * @param rateDouble
+     * @param time
+     * @return
+     */
     public static double calculateInterest(int principal, double rateDouble, int time) {
 
-//        double simpleInterest;
-//        Scanner scanner = new Scanner(System.in);
-//        System.out.print("Enter the principal:");
-//        principal = scanner.nextInt();
-//        System.out.print("Enter the rate as a percent without the % sign: ");
-//        int rate = scanner.nextInt();
-//        double rateDouble = (double) rate/100;
-//        System.out.print("Enter the time in years:");
-//        time = scanner.nextInt();
-//        System.out.println("Principal: " + principal + " rate: " + rateDouble + " time: " + time);
+        /**
+         * This will calculate the simple interest.
+         */
         double simpleInterest = principal*rateDouble*time;
 
+        /**
+         * This will print the simple interest to the console.
+         */
         System.out.println("Interest accrued after " + time + " years is: $" + simpleInterest);
-        return simpleInterest;
-    }
 
+        /**
+         * This will return the simple interest.
+         */
+        return simpleInterest;
+    } // end calculateInterest
+
+    /**
+     * This main method demonstrates the use of the calculateInterest method.
+     * @param args
+     */
     public static void main(String[] args) {
 
+        /**
+         * Three variables are declared: principal, rate, and time.
+         */
         int principal, rate, time;
-        double simpleInterest;
 
+        /**
+         * A Scanner object is declared to take in user input.
+         */
         Scanner scanner = new Scanner(System.in);
 
+        /**
+         * The system asks the user for input and assigns the values to the
+         * appropriate variable to use as parameters for the calculateInterest method.
+         */
         System.out.print("Enter the principal:");
         principal = scanner.nextInt();
         System.out.print("Enter the rate as a percent without the % sign: ");
         rate = scanner.nextInt();
+
+        /**
+         * This will cast the rate to a corresponding double to be used in
+         * the calculateInterest method.
+         */
         double rateDouble = (double) rate/100;
+
         System.out.print("Enter the time in years:");
         time = scanner.nextInt();
-        System.out.println("Principal: " + principal + " rate: " + rate + " time: " + time);
-//        simpleInterest = principal*rateDouble*time;
-//        System.out.println("Interest accrued after " + time + " years is: " + simpleInterest);
 
+        /**
+         * This will display the input from the console out to the console.
+         */
+        System.out.println("Principal: " + principal + " rate: " + rate + " time: " + time);
+
+        /**
+         * Here, we call the method calculateInterest using variables assigned by
+         * user input as parameters.
+         */
         calculateInterest(principal, rateDouble, time);
 
-    }
+    } // end main
 
-} // done
+} // end class
+// done
