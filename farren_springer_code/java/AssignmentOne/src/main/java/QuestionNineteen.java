a6a7d81b250eab8cb33f1c39b34cbb3c628a7e20
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-08-10 14:58:04 -0500

    q19 documentation done

--- farren_springer_code/java/AssignmentOne/src/main/java/QuestionNineteen.java
+++ farren_springer_code/java/AssignmentOne/src/main/java/QuestionNineteen.java
@@ -1,78 +1,140 @@
 import java.util.ArrayList;
 
+/**
+ * This class contains three methods that operate on an ArrayList and
+ * one main method to demonstrate their use.
+ */
+
 public class QuestionNineteen {
 
-    // Create a method to add the even numbers in an int array
+    /**
+     * This method will add all the even numbers in an ArrayList of integers and return the sum.
+     * @param intArr
+     * @return
+     */
     static int addEvenNumbers (ArrayList<Integer> intArr) {
+
+        /**
+         * An integer variable is initialized to zero.
+         */
         int result = 0;
 
-        // Parse through the int array
+        /**
+         * This loop will parse through every number in the ArrayList of
+         * Integer objects specified in the parameter of the method.
+         */
         for(int i = 0; i < intArr.size(); i++) {
-            // If a number is even, add it to result
+            /**
+             * If an integer is even, that integer will be added to the result.
+             */
             if(QuestionSix.isEven(intArr.get(i))) {
                 result += intArr.get(i);
             }
         }
+        /**
+         * The method returns the result, which is the sum of all even integers
+         * in the ArrayList.
+         */
         return result;
     }
 
-    // Create a method to add the odd numbers in an int array
+    /**
+     * This method will add all the odd numbers in an ArrayList of integers and return the sum.
+     * @param intArr
+     * @return
+     */
     static int addOddNumbers (ArrayList<Integer> intArr) {
         int result = 0;
 
-        // Parse through the array
+        /**
+         * This loop will parse through every number in the ArrayList of
+         * Integer objects specified in the parameter of the method.
+         */
         for (int i = 0; i < intArr.size(); i++) {
-            // If a number is odd, add it to result
+            /**
+             * If an integer is NOT even, that integer will be added to the result.
+             */
             if (!QuestionSix.isEven(intArr.get(i))) {
                 result += intArr.get(i);
             }
         }
+        /**
+         * The method returns the result, which is the sum of all odd integers
+         * in the ArrayList.
+         */
         return result;
     }
 
-    // Create a method to add the prime numbers
+    /**
+     * This method will remove all the prime numbers in an ArrayList and return the modified ArrayList.
+     * @param intArr
+     * @return
+     */
     static ArrayList<Integer> removePrimeNumbers (ArrayList<Integer> intArr) {
         int result = 0;
 
-        // Parse through the array
+        /**
+         * This loop will parse through every number in the ArrayList of
+         * Integer objects specified in the parameter of the method.
+         */
         for (int i = 0; i < intArr.size(); i++) {
+            /**
+             * If an integer is prime, that integer will be removed from the Arraylist
+             * specified in the parameter of the method.
+             */
             if(QuestionNine.isPrime(intArr.get(i))){
                 System.out.println(intArr.get(i) + " is prime.");
                 intArr.remove(i);
             }
         }
+        /**
+         * The method returns an ArrayList of Integer type, which is the sum of all odd integers
+         * in the ArrayList.
+         */
         return intArr;
     }
 
+    /**
+     * The main method shows the implementation of the three other methods in the class.
+     * @param args
+     */
     public static void main(String[] args) {
 
-       /*
-       Create an ArrayList and insert integers 1 through 10. Display
-       the ArrayList. Add all the even numbers up and display the
-       result. Add all the odd numbers up and display the result.
-       Remove the prime numbers from the ArrayList and print out
-       the remaining ArrayList.
-        */
-
-        // Create an ArrayList object
+        /**
+         * This will create an ArrayList object of type Integer to execute the
+         * three other methods in the class.
+         */
         ArrayList<Integer> theArray = new ArrayList<>();
 
-        // Add numbers 1 through 10 to the array
+        /**
+         * This will give the ArrayList values 1 through 10.
+         */
         for (int i = 0; i < 10; i++) {
             theArray.add(i + 1);
         }
+
+        /**
+         * This will output the original ArrayList.
+         */
         System.out.println("These are the Integer ArrayList object's values: " + theArray.toString());
 
 
-        // Print the sum of the even numbers
+        /**
+         * Here, we are showing the result of the addEvenNumbers method.
+         */
         System.out.println("Here is the sum of all of the even numbers: " + addEvenNumbers(theArray));
 
-        // Print the sum of the odd numbers
+        /**
+         * Here, we are showing the result of the addOddNumbers method.
+         */
         System.out.println("Here is the sum of all of the odd numbers: " + addOddNumbers(theArray));
 
-        // Print the sum of the prime numbers
+        /**
+         * Here, we are showing the result of the removePrimeNumbers method.
+         */
         System.out.println("Here is the original ArrayList without the prime numbers: " + removePrimeNumbers(theArray));
 
-    }
+    } // end main
 
-} // done
+} // end class
+// done
