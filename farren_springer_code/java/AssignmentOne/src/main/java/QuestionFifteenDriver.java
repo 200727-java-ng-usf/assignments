4b02467f920b7dd2ea745a95c55feff5c94f2a98
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-08-10 15:15:11 -0500

    q15 documentation done

--- farren_springer_code/java/AssignmentOne/src/main/java/QuestionFifteenDriver.java
+++ farren_springer_code/java/AssignmentOne/src/main/java/QuestionFifteenDriver.java
@@ -13,12 +13,21 @@ public class QuestionFifteenDriver {
         int yourNum1 = scanner.nextInt();
         System.out.println("Enter your second integer: ");
         int yourNum2 = scanner.nextInt();
+
+        /**
+         * This will create a QuestionFifteen object from user input.
+         */
         QuestionFifteen yourNum = new QuestionFifteen(yourNum1,yourNum2);
+
+        /**
+         * This will call the QuestionFifteen methods.
+         */
         yourNum.add();
         yourNum.subtract();
         yourNum.multiply();
         yourNum.divide();
 
 
-    }
-} // done
+    } // end main
+} // end class
+// done
