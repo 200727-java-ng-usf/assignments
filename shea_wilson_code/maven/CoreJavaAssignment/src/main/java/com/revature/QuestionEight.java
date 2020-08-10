3bb2d4cc437487250ca6f6642fce1bbd2ab73e96
Shea Wilson <sheawilsn@gmail.com>
2020-08-10 15:21:40 -0500

    Solved Q8

--- shea_wilson_code/maven/CoreJavaAssignment/src/main/java/com/revature/QuestionEight.java
+++ shea_wilson_code/maven/CoreJavaAssignment/src/main/java/com/revature/QuestionEight.java
@@ -1,25 +1,32 @@
 package com.revature;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.ListIterator;
 
 public class QuestionEight{
 
-	static String palin(String w){
+	//method that should retrieve the reverse of each word
+
+	public static String palin(String w){
 
 		String pa = w;
 
-		for(int i = 0 ; i < pa.charAt(pa.length()-1); --i){
-			System.out.println(pa.charAt(i));
+		// for loop to iterate through each char in each word from last to first
+		// and print the result
+
+		for(int i = pa.length()-1; i >=0  ; i --){
+			System.out.print(pa.charAt(i));
 		}
 
 		return " ";
 	}
 
 	public static void main(String[] args){
-		
+		// instantiated two ListIterators to read each name in the collection of first and second.
 		ListIterator<String> litr,litr2 ;
+		// Instantiated the ArrayList of first.
 		ArrayList<String> first = new ArrayList<>();
+
+		// Added the names to the first collection
 		first.add("karan");
 		first.add("madam");
 		first.add("tom");
@@ -35,22 +42,18 @@ public class QuestionEight{
 		
 		litr = first.listIterator();
 		System.out.println("First List: ");
-		while(litr.hasNext()){
-		System.out.println(litr.next());
+		while(litr.hasNext()){ //while there is something to read in the next spot keep going
+		System.out.println(litr.next()); //print the name
 		}
 
 		System.out.println("----------------------------------------");
 
-		ArrayList<String> second = new ArrayList<>(first);//
+		ArrayList<String> second = new ArrayList<>(first);// copy the first collection to the second
 		System.out.println("Second List: ");
 		litr2 = second.listIterator();
 		while(litr2.hasNext()){
-			System.out.println(litr2.next());
+			System.out.println(palin(litr2.next())); //print out the second list with the words in reverse.
 		}
-
-
-		
-
 	}
 
 }
