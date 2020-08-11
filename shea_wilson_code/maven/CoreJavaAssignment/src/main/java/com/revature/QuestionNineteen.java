204dcc9fd7111dc31fe69e1831adb044917df7b7
Shea Wilson <sheawilsn@gmail.com>
2020-08-11 13:07:32 -0500

    Begining of q19

---
+++ shea_wilson_code/maven/CoreJavaAssignment/src/main/java/com/revature/QuestionNineteen.java
@@ -0,0 +1,21 @@
+package com.revature;
+import java.util.ArrayList;
+import java.util.List;
+
+public class QuestionNineteen {
+
+    public static void main(String[] args) {
+
+        List<Integer> numbers = new ArrayList<>();
+        int num = 0;
+
+        for (int i = 0 ; i <= 10 ; i++){
+            numbers.add(num++);
+        }
+        System.out.println(numbers);
+
+
+
+
+    }
+}
