50035d40f5e7d0d9208cae2b8ecb1a4ba0ba27e6
SeanRog <seanmikerog@gmail.com>
2020-08-11 11:13:55 -0700

    finished with assignment 1

--- sean_rogers_code/Java/src/test/java/Question17Test.java
+++ sean_rogers_code/Java/src/test/java/Question17Test.java
@@ -1,52 +1,48 @@
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
-import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 
 public class Question17Test {
-    Question17 question17;
-    ByteArrayOutputStream outContent;
-    ByteArrayOutputStream errContent;
-    InputStream inStream;
-    PrintStream originalOut;
-    PrintStream originalErr;
-    InputStream originalIn;
-
-
-    @BeforeClass
-    public void setup() {
-        question17 = new Question17();
-        outContent = new ByteArrayOutputStream();
-        errContent = new ByteArrayOutputStream();
-        inStream = new ByteArrayInputStream(new byte[100]);
-        originalOut = System.out;
-        originalErr = System.err;
-        originalIn = System.in;
-    }
     @Test
-    public void question17Test() {
-        question17 = new Question17();
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-        InputStream inStream = new ByteArrayInputStream(new byte[100]);
-        PrintStream originalOut = System.out;
-        PrintStream originalErr = System.err;
-        InputStream originalIn = System.in;
-
-        System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
-        assert(question17.calculateInterest(5.0, 4.0, 3) == 60);
-
-    }
-
-    @Test
-    public void question17NegativeTest() {
+    public void principle56point3rate20point6years4() {
         Question17 question17 = new Question17();
+        String s = "56.3\n20.6\n4";
+        InputStream inContent = new ByteArrayInputStream(s.getBytes());
+        System.setIn(inContent);
+        assertEquals(question17.calculateInterest(), 4639.12, .0001);
 
-        assert(question17.calculateInterest(5.0, 4.0, 3) != 53.4);
+    }
+
+    @Test
+    public void principle5rate4years3() {
+        Question17 question17 = new Question17();
+        String s = "5.0\n4.0\n3";
+        InputStream inContent = new ByteArrayInputStream(s.getBytes());
+        System.setIn(inContent);
+        assertEquals(question17.calculateInterest(), 60.0, .0001);
+
+    }
+
+    @Test
+    public void principle6rate4years3() {
+        Question17 question17 = new Question17();
+        String s = "6.0\n4.0\n3";
+        InputStream inContent = new ByteArrayInputStream(s.getBytes());
+        System.setIn(inContent);
+        assertEquals(question17.calculateInterest(), 72.0, .0001);
+
+    }
+
+    @Test
+    public void negativeTestPrincipleIsLetterFRateIs4point0YearIs3() {
+        Question17 question17 = new Question17();
+        String s = "6.0\n4.0\n3";
+        InputStream inContent = new ByteArrayInputStream(s.getBytes());
+        System.setIn(inContent);
+        assertEquals(question17.calculateInterest(), 72.0, .0001);
 
     }
 }
