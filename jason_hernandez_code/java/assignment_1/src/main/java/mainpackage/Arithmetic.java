df234246a2a259176358b892eeb5061bedf8859e
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-10 19:32:02 -0700

    Test cases for Arithmetic

--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/Arithmetic.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/Arithmetic.java
@@ -2,7 +2,7 @@ package mainpackage;
 
 public interface Arithmetic {
     float firstNumber = 0;
-    float secondNumber = 0;
+    float secondNumber = 1;
 
     float addUp();
     float subtractIt();
--- jason_hernandez_code/java/assignment_1/src/test/java/ArithmeticTest.java
+++ jason_hernandez_code/java/assignment_1/src/test/java/ArithmeticTest.java
@@ -1,15 +1,20 @@
+import mainpackage.ArithmeticImpl;
 import org.junit.After;
-import org.junit.Assert;
+import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertArrayEquals;
+
 public class ArithmeticTest {
 
-    private ArithmeticTest sut;
+    private ArithmeticImpl sut;
+    float firstNumber = 5;
+    float secondNumber = 3;
 
     @Before
     public void setUp() {
-        sut = new ArithmeticTest();
+        sut = new ArithmeticImpl();
     }
 
     @After
@@ -17,5 +22,42 @@ public class ArithmeticTest {
         sut = null;
     }
 
+    @Test
+    public void tryAdding() {
+        String msg = "Should return 5 + 3 = 8";
+        int expectedNum = 8;
+        assertEquals(sut.addUp(), expectedNum, 0.01);
+    }
 
+    @Test
+    public void trySubtracting() {
+        String msg = "Should return 5 - 3 = 2";
+        int expectedNum = 2;
+        assertEquals(sut.subtractIt(), expectedNum, 0.01);
+    }
+
+    @Test
+    public void tryMutiplying() {
+        String msg = "Should return 5 * 3 = 15";
+        int expectedNum = 15;
+        assertEquals(sut.multiplyIt(), expectedNum, 0.01);
+    }
+
+    @Test
+    public void tryDividing() {
+        String msg = "Should return 5 / 3 = 2";
+        double expectedNum = 1.667;
+        assertEquals(sut.divideBy(), expectedNum, 0.01);
+    }
+
+    @Test
+    public void cantDivideByZero() {
+        try {
+            secondNumber = 0;
+            sut.divideBy();
+        } catch (ArithmeticException e) {
+            String msg = "Should throw ArithmeticException";
+        }
+    }
 }
+
