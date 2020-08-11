28382dc5fbbe51e49b0dff24c2219a09a3f532fb
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-10 19:48:18 -0700

    Test for FindMinimum

---
+++ jason_hernandez_code/java/assignment_1/src/test/java/MinimumTester.java
@@ -0,0 +1,48 @@
+import mainpackage.BubbleSort;
+import mainpackage.FindMinimum;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MinimumTester {
+    private FindMinimum sut;
+
+    @Before
+    public void setUp() {
+        sut = new FindMinimum();
+    } // Call to the class being tested
+
+    @After
+    public void tearDown() {
+        sut = null;
+    } // Reset at the end
+
+    @Test
+    public void checkFirstNumberGreater() {
+        int num1 = 6;
+        int num2 = 4;
+        int expectedOutput = 6;
+        assertEquals(sut.findTheMinimum(num1, num2), expectedOutput, 0.01);
+    }
+
+    @Test
+    public void checkSecondNumberGreater() {
+        int num1 = 4;
+        int num2 = 6;
+        int expectedOutput = 4;
+        assertEquals(sut.findTheMinimum(num1, num2), expectedOutput, 0.01);
+    }
+
+    @Test
+    public void checkNumbersEqual() {
+        int num1 = 6;
+        int num2 = 6;
+        int expectedOutput = 6;
+        try {
+            assertEquals(sut.findTheMinimum(num1, num2), expectedOutput, 0.01);
+        } catch (ArithmeticException e) {
+            String msg = "Should throw ArithmeticException";
+        }
+    }
+}
