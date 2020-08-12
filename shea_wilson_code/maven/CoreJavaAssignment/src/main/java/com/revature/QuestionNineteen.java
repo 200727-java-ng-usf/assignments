5b5003571b081515e87198f6f691b48d2b548a5f
Shea Wilson <sheawilsn@gmail.com>
2020-08-11 22:35:14 -0500

    q19 additional stuff

--- shea_wilson_code/maven/CoreJavaAssignment/src/main/java/com/revature/QuestionNineteen.java
+++ shea_wilson_code/maven/CoreJavaAssignment/src/main/java/com/revature/QuestionNineteen.java
@@ -4,6 +4,37 @@ import java.util.List;
 
 public class QuestionNineteen {
 
+    static public int sumOfEvenNumbers(List<Integer> x){
+
+        int sum = 0;
+
+        for (int i = x.indexOf(0) ; i <= x.size(); i++){
+            if(x.indexOf(i) % 2 == 0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+
+    static public int sumOfOddNumbers(List<Integer> x){
+
+        int sum = 0;
+
+        for (int i = x.indexOf(0) ; i <= x.size(); i++){
+            if(x.indexOf(i) % 2 != 0){
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+
+    static public boolean checkIfPrime(List<Integer> num){
+        for (int x = 2; x < (int)Math.sqrt(num.indexOf(x)); x++)
+            if (num.indexOf(x) % x == 0)
+                return false;
+        return true;
+    }
+
     public static void main(String[] args) {
 
         List<Integer> numbers = new ArrayList<>();
@@ -12,10 +43,21 @@ public class QuestionNineteen {
         for (int i = 0 ; i <= 10 ; i++){
             numbers.add(num++);
         }
+
         System.out.println(numbers);
 
+        System.out.println("Here are your even numbers total "+ sumOfEvenNumbers(numbers));
+
+        System.out.println("Here are your odd numbers total "+ sumOfOddNumbers(numbers));
+
+        if (checkIfPrime(numbers)){
+
+            numbers.remove(numbers);
+
+        }
 
 
 
-    }
+
+        }
 }
