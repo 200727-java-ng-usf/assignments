c9fed63ba80cac669c55a734f337568ca4018c91
Farren Springer <68714743+fjs507@users.noreply.github.com>
2020-08-12 09:06:19 -0500

    Negative test for q16 done

--- farren_springer_code/java/AssignmentOne/src/test/java/QuestionSixteenTest.java
+++ farren_springer_code/java/AssignmentOne/src/test/java/QuestionSixteenTest.java
@@ -3,6 +3,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class QuestionSixteenTest {
 
@@ -14,10 +15,22 @@ public class QuestionSixteenTest {
     @After
     public void tearDown() throws Exception { sut = null; }
 
+    // Positive test
     @Test
     public void thereAreFiveLettersInHello() {
         int expectedResult = 5;
         int actualResult = sut.numberOfCharactersInString("Hello");
         assertEquals(expectedResult, actualResult);
     }
+
+    // Negative test
+    @Test (expected = NullPointerException.class)
+        public void nullValuePassedToNumberOfCharactersInStringThrowsNullPointerException() {
+        String nullString = null;
+        int unexpectedResult = 4;
+        int actualResult = sut.numberOfCharactersInString(nullString);
+        assertNotEquals(unexpectedResult, actualResult);
+    }
+
+
 }
