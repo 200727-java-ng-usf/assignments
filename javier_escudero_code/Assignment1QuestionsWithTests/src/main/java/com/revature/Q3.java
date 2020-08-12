7109ff50e92fa044044ca49d5313db162a91aeb6
escuderj <javierusn@yahoo.com>
2020-08-12 14:14:39 -0400

    Q3 and test work

--- javier_escudero_code/Assignment1QuestionsWithTests/src/main/java/com/revature/Q3.java
+++ javier_escudero_code/Assignment1QuestionsWithTests/src/main/java/com/revature/Q3.java
@@ -1,32 +1,68 @@
 package com.revature;
-
 public class Q3 {
-    public String reverseString(String str) {
-        // save the string length for future use.
-        int stringLength = str.length();      // If "abc", length = 3
-        int startingIndex = stringLength - 1;  // If "abc", startingIndex = 2
+
+    public String reverseMethod(StringBuilder sb)
+    {
+        //System.out.println(sb);	//i.e.passed String  "abc"
+        int initialLength = sb.length(); //3: a=[0], b=[1], c=[2]
+        final int LAST_INDEX= initialLength - 1; //[2] constant to delete half of mirrored string
+
+        /*
+         * appends a reverse copy at the beginning
+         * for example: "abc", the LAST_INDEX character copied into position i++,
+         *  becoming cba.The result is a mirrored string: "cbaabc"
+         * */
+        for(int i = 0; i < initialLength; i++ )
+            sb.insert(i, sb.charAt(LAST_INDEX)); // LAST_INDEX constant to delimit size
+        //System.out.println(sb);
 
 
-        // Create a char array of str.length() characters.
-        // ... Add all letters to it.
-        char[] reverseStr = new char[stringLength];
+        /*
+         *  Deletes the original (not reversed) copy, the second half
+         * */
+        for(int i = 0; i < initialLength; i++ )
+            sb.deleteCharAt(initialLength); // "cbaabc"
+        //System.out.println(sb);
 
-        //reverses the order of the char list
-        // starting from the highest index: startingIndex
-        /** charAt(int index) method returns the character at the specified index in a string.
-         The index value that we pass in this method should be between 0 and (length of string-1) */
-        for (int i = startingIndex; i > -1; i--) {
-            reverseStr[startingIndex - i] = (str.charAt(i));    // returns character from highest index to lower
-        }
-
-        //puts reversed char list back into a string
-        System.out.println("" + String.valueOf(reverseStr));
-        return str;
+        // toString() needed, otherwise it cannot convert from StringBuilder to String
+        return(sb.toString()); // returns a string
     }
 
-//    public static void main(String[] args) {
-//        Q3 q3 = new Q3();
-//        q3.reverseString("abc d123");
+//		public static void main(String[] args) {
+//			Q3b q3b = new Q3b(); // instantiating creating a class object
+//			StringBuilder sb = new StringBuilder("Hello ");  // creating the string
+//			q3b.reverseMethod(sb); // reverse the string
+//			System.out.println("in main" + sb);
 //
-//    }
+//	}
+
 }
+
+//
+//public class Q3 {
+//    public String reverseString(String str) {
+//        System.out.println("Before reversing " + str);
+//        // save the string length for future use.
+//        int stringLength = str.length();      // If "abc", length = 3
+//        int startingIndex = stringLength - 1;  // If "abc", startingIndex = 2
+//
+//
+//        // Create a char array of str.length() characters.
+//        // ... Add all letters to it.
+//        char[] reverseStr = new char[stringLength];
+//
+//        //reverses the order of the char list
+//        // starting from the highest index: startingIndex
+//        /** charAt(int index) method returns the character at the specified index in a string.
+//         The index value that we pass in this method should be between 0 and (length of string-1) */
+//        for (int i = startingIndex; i > -1; i--) {
+//            reverseStr[startingIndex - i] = (str.charAt(i));    // returns character from highest index to lower
+//        }
+//
+//        //puts reversed char list back into a string
+//        System.out.println("from Q3");
+//        System.out.println(String.valueOf(reverseStr));
+//        System.out.println("end Q3, now return string");
+//        return str;
+//    }
+//}
--- javier_escudero_code/Assignment1QuestionsWithTests/src/test/java/com/revature/Q3Test.java
+++ javier_escudero_code/Assignment1QuestionsWithTests/src/test/java/com/revature/Q3Test.java
@@ -1,30 +1,56 @@
 package com.revature;
 
-import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-import static org.junit.Assert.assertSame;
 
 public class Q3Test {
-    private Q3 sut; // SUT = System Under Test
-   // delete public static void main(String[] args) {
 
-   @Before
-   public void t1Setup(){
-       sut = new Q3();
-   }
-
+    private Q3 q3; // SUT = System Under Test
     @Test
-    public void expectedString() {
-        // string to be passed
-      //  String stringToBePassed = sut.reverseString("abc d123");
-
-        // string expected
-        String  expectedReversedString= "321d abc";
-
-        assertSame(expectedReversedString, sut.reverseString("abc d123") );
+    public void testQ3() {
+        q3 = new Q3(); // creating a class object
+        StringBuilder sb = new StringBuilder("abc");
+        System.out.println("String was: " + sb);
+        q3.reverseMethod(sb);
+        System.out.println("\nAfter reversing it:\n" + sb);
     }
-    //}
-
+//	public static void main(String[] args) {
+//		//StringBuffer sb = new StringBuffer("Hello ");
+//		Q3a q3a = new Q3a(); // creating a class object
+//		StringBuffer sb = new StringBuffer("Hello ");
+//		q3a.reverseMethod(sb);
+//		System.out.println("in main" + sb);
+//	}
 }
+
+//package com.revature;
+//
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import static org.junit.Assert.*;
+//import static org.junit.Assert.assertSame;
+//
+//public class Q3Test {
+//    private Q3 sut; // SUT = System Under Test
+//   // delete public static void main(String[] args) {
+//
+//   @Before
+//   public void t1Setup(){
+//       sut = new Q3();
+//   }
+//
+//    @Test
+//    public void expectedString() {
+//        // string to be passed
+//      //  String stringToBePassed = sut.reverseString("abc d123");
+//
+//        // string expected
+//        String  expectedReversedString= "321d abc";
+//        System.out.println("Q3Testing" + );
+//        System.out.println(sut.reverseString("abc d123") );
+//     //   assertSame(expectedReversedString, sut.reverseString("abc d123") );
+//    }
+//    //}
+//
+//}
