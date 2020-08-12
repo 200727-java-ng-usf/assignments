7854c736f9b4bbb16b2e6da9a7006afe946846a7
escuderj <javierusn@yahoo.com>
2020-08-12 14:15:52 -0400

    Q3Test edited, removed commented out part

--- javier_escudero_code/Assignment1QuestionsWithTests/src/test/java/com/revature/Q3Test.java
+++ javier_escudero_code/Assignment1QuestionsWithTests/src/test/java/com/revature/Q3Test.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 public class Q3Test {
 
     private Q3 q3; // SUT = System Under Test
+
     @Test
     public void testQ3() {
         q3 = new Q3(); // creating a class object
@@ -14,43 +15,4 @@ public class Q3Test {
         q3.reverseMethod(sb);
         System.out.println("\nAfter reversing it:\n" + sb);
     }
-//	public static void main(String[] args) {
-//		//StringBuffer sb = new StringBuffer("Hello ");
-//		Q3a q3a = new Q3a(); // creating a class object
-//		StringBuffer sb = new StringBuffer("Hello ");
-//		q3a.reverseMethod(sb);
-//		System.out.println("in main" + sb);
-//	}
 }
-
-//package com.revature;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import static org.junit.Assert.*;
-//import static org.junit.Assert.assertSame;
-//
-//public class Q3Test {
-//    private Q3 sut; // SUT = System Under Test
-//   // delete public static void main(String[] args) {
-//
-//   @Before
-//   public void t1Setup(){
-//       sut = new Q3();
-//   }
-//
-//    @Test
-//    public void expectedString() {
-//        // string to be passed
-//      //  String stringToBePassed = sut.reverseString("abc d123");
-//
-//        // string expected
-//        String  expectedReversedString= "321d abc";
-//        System.out.println("Q3Testing" + );
-//        System.out.println(sut.reverseString("abc d123") );
-//     //   assertSame(expectedReversedString, sut.reverseString("abc d123") );
-//    }
-//    //}
-//
-//}
