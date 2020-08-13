1b7906479e1e8571e0e69f76c3ae8ebbd8c33a3f
escuderj <javierusn@yahoo.com>
2020-08-12 22:13:26 -0400

    passed 2 tests

--- javier_escudero_code/Assignment1QuestionsWithTests/src/test/java/com/revature/Q2Test.java
+++ javier_escudero_code/Assignment1QuestionsWithTests/src/test/java/com/revature/Q2Test.java
@@ -16,15 +16,27 @@ public class Q2Test {
 
     @Test
     public void testFibonacciNumbers() {
-       int[] arrayExpected = sut.first25FibonacciNumbers(25); // returns an array with 25 Fibonacci numbers
-        //assertEquals(arrayExpected, sut.first25FibonacciNumbers(20));
-        int i=0;
-        for(i = 0; i < 25; i++)
-            System.out.print( " " + arrayExpected[i] + " ");
-        System.out.println("\nThe first " + i + " Fibonacci numbers." );
+        int[] arrayExpected = { 0 ,1, 1,  2,  3,
+                                5,  8,  13,  21,  34,
+                                55,  89,  144,  233, 377,
+                                610,  987,  1597,  2584,  4181,
+                                6765,  10946,  17711,  28657,  46368 };
+        int i = 0;
+        for (; i < 25; i++)
+            System.out.print(" " + arrayExpected[i] + " ");
+        System.out.println("\nThe first " + i + " Fibonacci numbers.");
         assertArrayEquals(arrayExpected, sut.first25FibonacciNumbers(25));
     }
-}
+
+    @Test
+    public void specificNumber() {
+        int [] arr = new int[25];
+        arr[24] = 46368;
+        int[] actual = sut.first25FibonacciNumbers(25);
+        assertEquals(arr[24], actual[24]);
+        }
+    }
+
 
 //
 //
