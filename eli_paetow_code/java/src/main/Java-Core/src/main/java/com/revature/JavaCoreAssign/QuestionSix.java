fafa4af80de6e76954a2dcbe2b980e65b1d99aa0
EPaetow <elipaetow@yahoo.com>
2020-08-12 21:29:10 -0500

    positive test works

--- eli_paetow_code/java/src/main/Java-Core/src/main/java/com/revature/JavaCoreAssign/QuestionSix.java
+++ eli_paetow_code/java/src/main/Java-Core/src/main/java/com/revature/JavaCoreAssign/QuestionSix.java
@@ -1,4 +1,16 @@
 package com.revature.JavaCoreAssign;
 
+//Q6. Write a program to determine if an integer is even without using the modulus operator (%)
 public class QuestionSix {
+
+    static boolean numIsEven(int num){
+
+        //take the number passsed from test class
+        //divide by 2 then * 2
+        // 10/2 = 5. 5*2 = 10. 10 = even
+        return ((num /2 ) * 2 ==num);
+
+
+    }
+
 }
--- eli_paetow_code/java/src/main/Java-Core/src/test/java/com/revature/JavaCoreAssign/QuestionSixTest.java
+++ eli_paetow_code/java/src/main/Java-Core/src/test/java/com/revature/JavaCoreAssign/QuestionSixTest.java
@@ -1,4 +1,32 @@
 package com.revature.JavaCoreAssign;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotEquals;
+
 public class QuestionSixTest {
+
+    private QuestionSix sut;
+
+    @Before
+    public void setup(){
+        sut = new QuestionSix();
+    }
+
+    @Test
+    public void returnEvenGood(){
+        //assemble
+        boolean expectedResult = true;
+        boolean actualResult = sut.numIsEven(10);
+        //act
+        //assert
+        assertNotEquals(false, actualResult);
+    }
+
+    @After
+    public void tearDown(){
+        sut =null;
+    }
 }
