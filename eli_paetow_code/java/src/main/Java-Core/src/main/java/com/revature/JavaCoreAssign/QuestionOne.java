ef36c45965f1116cb889132ce707c8d1ad3d105f
EPaetow <elipaetow@yahoo.com>
2020-08-12 20:10:42 -0500

    added arrange act assert

--- eli_paetow_code/java/src/main/Java-Core/src/main/java/com/revature/JavaCoreAssign/QuestionOne.java
+++ eli_paetow_code/java/src/main/Java-Core/src/main/java/com/revature/JavaCoreAssign/QuestionOne.java
@@ -1,4 +1,33 @@
 package com.revature.JavaCoreAssign;
 
+
+import java.util.Arrays;
+
+// Perform a bubble sort on the following integer array:  1,0,5,6,3,2,3,7,9,8,4
 public class QuestionOne {
+
+    public int[] bubbleSortQ1 (int[] arr){
+
+        if (arr == null || arr.length ==0){
+            return new int[0];
+        }
+
+        int[] arrayCopy = Arrays.copyOf(arr, arr.length);
+
+        for (int i =0; i<arrayCopy.length; i++){
+
+            for (int j=0; j< arrayCopy.length -i && arrayCopy[j] != arrayCopy[arrayCopy.length -1]; j++){
+
+                if (arrayCopy[j] > arrayCopy[j+1]){
+                    int x = arrayCopy[j];
+                    arrayCopy[j] = arrayCopy[j +1];
+                    arrayCopy[j+1] = x;
+                }
+            }
+        }
+
+        return arrayCopy;
+
+
+    }
 }
--- eli_paetow_code/java/src/main/Java-Core/src/test/java/com/revature/JavaCoreAssign/QuestionOneTest.java
+++ eli_paetow_code/java/src/main/Java-Core/src/test/java/com/revature/JavaCoreAssign/QuestionOneTest.java
@@ -1,4 +1,43 @@
 package com.revature.JavaCoreAssign;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+
 public class QuestionOneTest {
+    //test test
+    //always do this first
+    private QuestionOne questionOne;
+
+    //first test to set up
+    @Before
+    public void setup(){
+        questionOne = new QuestionOne();
+    }
+
+
+
+    //actual test
+    @Test
+    public void bubbleSortTestGood(){
+        //arrange
+        int[] unsortedArray = { 1,0,5,6,3,2,3,7,9,8,4};
+        int[] sortedArray =  {0,1,2,3,3,4,5,6,7,8,9};
+        //act
+
+        //assert
+        assertArrayEquals(sortedArray, questionOne.bubbleSortQ1(sortedArray));
+
+    }
+
+    //need negative test
+
+    //reset test
+    @After
+    public void tearDown(){
+        questionOne =null;
+    }
+
+
 }
