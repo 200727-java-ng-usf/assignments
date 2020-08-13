2b74589a3c5c3da0c6889f5250234bad89685e6d
capolimia <capolimia@gmail.com>
2020-08-12 23:41:13 -0400

    up to q9 unit test

--- mia_capoli_code/Java/Assigment1/src/main/java/com/revature/questions/Question9.java
+++ mia_capoli_code/Java/Assigment1/src/main/java/com/revature/questions/Question9.java
@@ -7,31 +7,28 @@ public class Question9 {
     ArrayList<Integer> numbers = new ArrayList<>();
     ArrayList<Integer> primeNumbers = new ArrayList<>();
 
-    public void printPrime() {
+    public ArrayList<Integer> printPrime(int size) {
 
-        for (int i = 1; i <= 100; i++) {
+        for (int i = 1; i <= size; i++) {
             numbers.add(i);
-        }
-        for (int i = 0; i < 100; i++) {
-            if (numbers.get(i) <= 1) {
-                continue;
-            } else if (numbers.get(i) < 6) {
-                for (int j = 2; j < 6; j++) {
-                    if ((numbers.get(i) % j) != 0) {
-                        primeNumbers.add(numbers.get(i));
-                    }
-                    break;
-                }
-            } else {
-                for (int j = 2; j < i / 2; j++) {
-                    if ((numbers.get(i) % j) != 0) {
-                        primeNumbers.add(numbers.get(i));
-                    }
-                    break;
 
+        }
+
+        for (int i = 0; i < size; i++) {
+            if (numbers.get(i) == 1) {
+                continue;
+            } else {
+                if (numbers.get(i) == 2 || numbers.get(i) == 3 || numbers.get(i) == 5 || numbers.get(i) == 7){
+                    primeNumbers.add(numbers.get(i));
+                    continue;
+                } else if ((numbers.get(i) % 2) == 0 || (numbers.get(i) % 3) == 0 || (numbers.get(i) % 5) == 0 || (numbers.get(i) % 7 == 0)) {
+                    continue;
                 }
+                primeNumbers.add(numbers.get(i));
             }
         }
         System.out.println(primeNumbers);
+        return primeNumbers;
     }
+
 }
--- mia_capoli_code/Java/Assigment1/src/test/java/com/revature/testing/Question9Test.java
+++ mia_capoli_code/Java/Assigment1/src/test/java/com/revature/testing/Question9Test.java
@@ -1,4 +1,58 @@
 package com.revature.testing;
 
+import com.revature.questions.Question9;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.rules.Timeout;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertNotNull;
+
 public class Question9Test {
+
+    private Question9 question9;
+
+    @Rule
+    public ExpectedException expectedException = ExpectedException.none();
+
+    @Rule
+    public Timeout maximumTime = Timeout.seconds(3);
+
+    @Before
+    public void setup() {
+        question9 = new Question9();
+    }
+
+    //testsolutionwithreversedArray
+    //testsolutionwithpresortedarray
+    //testsolutionwithemptyarray
+
+    @Test
+    public void testPrimeNumbersGivenArray() {
+        String msg = "If a number is provided, the impl should return an array of numbers in a range of 1 to said number.";
+        int size = 100;
+        Object[] correctArray = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+        Object[] arrayToBeTested = question9.printPrime(size).toArray();
+        assertArrayEquals(msg, correctArray, arrayToBeTested);
+
+    }
+
+    @Test
+    public void testSolutionWithNullArray() {
+        String msg = "If a null array is provided, then impl should return an empty array";
+        Object[] expectedArray = {};
+        Object[] actualResult = question9.printPrime(0).toArray();
+        assertNotNull(actualResult);
+        assertArrayEquals(msg, expectedArray, actualResult);
+
+    }
+
+    @After
+    public void tearDown() {
+        question9 = null;
+    }
 }
+
