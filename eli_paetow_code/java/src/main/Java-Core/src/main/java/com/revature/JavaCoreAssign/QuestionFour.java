a129bd48a4cd99a6f82c16358847fd1b7f1ae14b
EPaetow <elipaetow@yahoo.com>
2020-08-12 21:07:27 -0500

    added comments to 5

--- eli_paetow_code/java/src/main/Java-Core/src/main/java/com/revature/JavaCoreAssign/QuestionFour.java
+++ eli_paetow_code/java/src/main/Java-Core/src/main/java/com/revature/JavaCoreAssign/QuestionFour.java
@@ -1,4 +1,15 @@
 package com.revature.JavaCoreAssign;
 
 public class QuestionFour {
+
+    public static int factorialQ4(int num){
+
+        if (num ==0)
+            return 1;
+
+
+        return num * factorialQ4(num -1);
+
+    }
+
 }
--- eli_paetow_code/java/src/main/Java-Core/src/test/java/com/revature/JavaCoreAssign/QuestionFourTest.java
+++ eli_paetow_code/java/src/main/Java-Core/src/test/java/com/revature/JavaCoreAssign/QuestionFourTest.java
@@ -1,4 +1,36 @@
 package com.revature.JavaCoreAssign;
 
+import org.junit.Before;
+import org.junit.After;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
 public class QuestionFourTest {
+    //starting with testing first
+    private QuestionFour sut;
+
+    @Before
+    public void setup(){
+        sut = new QuestionFour();
+    }
+
+    @Test
+    public void factorialGood(){
+        //assemble
+        int expectedResult = 120;
+        int actualResult = sut.factorialQ4(5);
+        //act
+        //assert
+        System.out.println("the factorial of 5 is: " + QuestionFour.factorialQ4(5));
+        assertEquals(expectedResult, actualResult);
+
+    }
+
+    @After
+    public void tearDown(){
+        sut = null;
+    }
 }
