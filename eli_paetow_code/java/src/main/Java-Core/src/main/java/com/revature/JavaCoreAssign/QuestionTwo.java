745f2cd85c6633397f981328bc55aead67d10e4f
EPaetow <elipaetow@yahoo.com>
2020-08-12 20:46:46 -0500

    need to set up test cases

--- eli_paetow_code/java/src/main/Java-Core/src/main/java/com/revature/JavaCoreAssign/QuestionTwo.java
+++ eli_paetow_code/java/src/main/Java-Core/src/main/java/com/revature/JavaCoreAssign/QuestionTwo.java
@@ -1,4 +1,28 @@
 package com.revature.JavaCoreAssign;
 
+//Write a program to display the first 25 Fibonacci numbers beginning at 0.
 public class QuestionTwo {
+
+    public static void fibonacciQ2(int num){
+        int num1=0;
+        int num2=1;
+
+        //if number is less than one dont return anything
+        if (num < 1)
+            return;
+
+        for (int i =1; i <=num; i++){
+            System.out.println(num2 + "");
+            int next = num1 + num2;
+            num1 = num2;
+            num2= next;
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        fibonacciQ2(25);
+    }
+
 }
--- eli_paetow_code/java/src/main/Java-Core/src/test/java/com/revature/JavaCoreAssign/QuestionTwoTest.java
+++ eli_paetow_code/java/src/main/Java-Core/src/test/java/com/revature/JavaCoreAssign/QuestionTwoTest.java
@@ -1,4 +1,28 @@
 package com.revature.JavaCoreAssign;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class QuestionTwoTest {
+
+//    private QuestionTwo sut;
+//
+//    @Before
+//    public void setup(){
+//        QuestionTwo questionTwo = new QuestionTwo();
+//    }
+//    @After
+//    public void tearDown(){
+//        questionTwo = null;
+//    }
+//    @Test
+//    public void fibonacciTestGood{
+//        //assemble
+//        int[] expectedResult = new int[]{0,1,1};
+//        int[] actualresult = QuestionTwo.fibonacciQ2(3);
+//        //act
+//        //assert
+//    }
+
 }
