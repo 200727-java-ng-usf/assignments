67ea5b2a90d6b8c1644a6ddce77f6792dc0bc82b
Shea Wilson <sheawilsn@gmail.com>
2020-08-14 13:42:52 -0500

    testing in work

--- shea_wilson_code/maven/CoreJavaAssignment/src/main/java/com/revature/QuestionTwo.java
+++ shea_wilson_code/maven/CoreJavaAssignment/src/main/java/com/revature/QuestionTwo.java
@@ -1,27 +1,38 @@
 package com.revature;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuestionTwo {
 
-    public static void fib(int n ){
 
+
+    public static ArrayList<Integer> fib(int n ){
+        List<Integer> list = new ArrayList<>();
         int f1 = 0, f2 = 1, i;
 
         if (n < 1)
-            return;
+            return null;
 
         for (i = 1; i <= n; i++)
         {
-            System.out.print(f2+" ");
+
+//            System.out.print(f2+" ");
             int next = f1 + f2;
             f1 = f2;
             f2 = next;
+            list.add(f2);
+
         }
+        System.out.println(list);
+        return null;
+
     }
 
     public static void main(String[] args){
 
 
-        fib(25);
+        fib(5);
         
 
     }
--- shea_wilson_code/maven/CoreJavaAssignment/src/test/java/com/revature/QuestionTwoTest.java
+++ shea_wilson_code/maven/CoreJavaAssignment/src/test/java/com/revature/QuestionTwoTest.java
@@ -1,4 +1,34 @@
 package com.revature;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertArrayEquals;
+
 public class QuestionTwoTest {
+
+    private QuestionTwo sut;
+
+    @Before
+    public void setUp(){
+        sut = new QuestionTwo();
+    }
+
+    @Test
+    public void testFib(){
+        String msg = "Which a given number, provide the fibonacci sequence up to that number.";
+        int num = 5;
+//        List<Integer> expectedResult = new ArrayList<>() {1, 2, 3, 5, 8};
+//        assertArrayEquals(msg, expectedResult, List.toArray(sut.fib(num)));
+
+    }
+
+    @After
+    public void tearDown(){
+        sut = null;
+    }
 }
