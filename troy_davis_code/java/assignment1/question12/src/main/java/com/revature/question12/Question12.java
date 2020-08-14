ee0444695395313a31a788b202fc0dc5042a8b25
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-08-13 20:19:30 -0400

    finishing question12, almost done just need a few more test cases

--- troy_davis_code/java/assignment1/question12/src/main/java/com/revature/question12/Question12.java
+++ troy_davis_code/java/assignment1/question12/src/main/java/com/revature/question12/Question12.java
@@ -6,36 +6,47 @@
 
 package com.revature.question12;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Question12 {
     //class fields to hold the array and range values
-    private int[] initialArray;
+    private int[] evenOnlyArray;
     private int rangeStart;
     private int rangeEnd;
-    private ArrayList<Integer> arrayList;
+    private int[] initialArray;
 
+    //no-args constructor to print dummy even values from 1-100
+    public Question12(){
+        this.rangeStart = 1;
+        this.rangeEnd = 100;
+        this.initialArray = new int[rangeEnd];
+        initialArray = getInitialArray(rangeStart, rangeEnd);
+        printThisArray(initialArray);
+    }
     //parameterized constructor to store and sort the arrays based on given range values:
     public Question12(int rangeStart, int rangeEnd){
         //set the given range start
         this.rangeStart = rangeStart;
         //set the given range end
-        this.rangeEnd = rangeEnd+1;
-        //initialize the array to store the values in
-        this.initialArray = new int[this.rangeEnd];
-        //fill the initial array based on given range values
-        for (int i = this.rangeStart; i < this.rangeEnd; i++){
-            initialArray[i] = i;
-        }
+        this.rangeEnd = rangeEnd;
+        //fill the initial array and store it
+        this.initialArray = getInitialArray(rangeStart, rangeEnd);
         //print the array now that it has been filled
         printThisArray(initialArray);
 
-
+    }
+    //method to fill the initial array based on given ranges
+    public int[] getInitialArray(int rangeStart, int rangeEnd){
+        int[] initialArray = new int[rangeEnd+1];
+        //fill the initial array based on given range values
+        for (int i = rangeStart; i <= rangeEnd; i++){
+            initialArray[i] = i;
+        }
+        return initialArray;
     }
 
     //method to print the array
-    private void printThisArray(int[] initialArray){
+    public void printThisArray(int[] initialArray){
         //using enhanced for loop
         for(int i : initialArray){
             if (i%2 == 0 && i != 0){ //make sure we do not print zero!
@@ -48,39 +59,4 @@ public class Question12 {
     }
 
 
-    //Getter and setter Methods
-    public int[] getInitialArray() {
-        return initialArray;
-    }
-
-    public void setInitialArray(int[] initialArray) {
-        this.initialArray = initialArray;
-    }
-
-    public int getRangeStart() {
-        return rangeStart;
-    }
-
-    public void setRangeStart(int rangeStart) {
-        this.rangeStart = rangeStart;
-    }
-
-    public int getRangeEnd() {
-        return rangeEnd;
-    }
-
-    public void setRangeEnd(int rangeEnd) {
-        this.rangeEnd = rangeEnd;
-    }
-
-    //Method to print the instance
-
-    @Override
-    public String toString() {
-        return "Question12{" +
-                "initialArray=" + Arrays.toString(initialArray) +
-                ", rangeStart=" + rangeStart +
-                ", rangeEnd=" + rangeEnd +
-                '}';
-    }
 }
--- troy_davis_code/java/assignment1/question12/src/test/java/com/revature/question12/Question12Test.java
+++ troy_davis_code/java/assignment1/question12/src/test/java/com/revature/question12/Question12Test.java
@@ -6,39 +6,49 @@
 
 package com.revature.question12;
 
-import com.revature.question12.Question12;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class Question12Test {
 
 
     //create an instance of the class to test
     Question12 sut;
+    private final PrintStream printStream = System.out;
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    String expectedReturn;
+    String msg;
+
 
     @Before
     public void setup(){
-        sut = new Question12(1, 100);
+        System.setOut(new PrintStream(outputStream));
+        expectedReturn = null;
+        msg = null;
     }
 
     @After
     public void tearDown(){
         sut = null;
+        System.setOut(printStream);
     }
 
     @Test
     public void notNullTest(){
-        String msg = "make sure neither array is not null";
-        Assert.assertTrue(sut.getInitialArray() != null);
+        msg = "make sure neither array is not null";
+        sut = new Question12();
     }
 
     @Test
     public void testForNumberOfElements(){
         String msg = "When given an array, does the even only array have the correct number or elements";
-        int number = sut.getInitialArray().length;
-        Assert.assertEquals(50, number);
+        //int number = sut.getEvenOnlyArray().length;
+        //Assert.assertEquals(50, number);
     }
 
 }
