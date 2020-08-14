82b3220ab831d3f0f72fc0f48e8c3e1e23d86a2b
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-08-13 21:18:50 -0400

    finished question12 came up with 3 unit tests

--- troy_davis_code/java/assignment1/question12/src/main/java/com/revature/question12/Question12.java
+++ troy_davis_code/java/assignment1/question12/src/main/java/com/revature/question12/Question12.java
@@ -6,37 +6,63 @@
 
 package com.revature.question12;
 
+
 import java.util.Arrays;
+import java.util.Objects;
 
 public class Question12 {
-    //class fields to hold the array and range values
-    private int[] evenOnlyArray;
-    private int rangeStart;
-    private int rangeEnd;
+    /**
+     * class fields to hold the array and range values
+     */
+    private Integer rangeStart;
+    private Integer rangeEnd;
     private int[] initialArray;
 
-    //no-args constructor to print dummy even values from 1-100
+    /**
+     * no-args constructor to print dummy even values from 1-100
+     */
     public Question12(){
+
         this.rangeStart = 1;
         this.rangeEnd = 100;
         this.initialArray = new int[rangeEnd];
         initialArray = getInitialArray(rangeStart, rangeEnd);
         printThisArray(initialArray);
     }
-    //parameterized constructor to store and sort the arrays based on given range values:
-    public Question12(int rangeStart, int rangeEnd){
-        //set the given range start
+
+    /**
+     * parameterized constructor to store and sort the arrays based on given range values:
+     * @param rangeStart
+     * @param rangeEnd
+     */
+    public Question12(Integer rangeStart, Integer rangeEnd){
+        //if the ranges are null, do nothing
+        if (rangeEnd == null || rangeStart == null){
+            return;
+        }
+        //if the ranges are zero, do nothing
+        if (rangeStart == 0 || rangeEnd == 0){
+            return;
+        }
+        //make sure range start is not less than range end
+        if (rangeEnd > rangeStart){
+            return;
+        }
         this.rangeStart = rangeStart;
-        //set the given range end
         this.rangeEnd = rangeEnd;
-        //fill the initial array and store it
-        this.initialArray = getInitialArray(rangeStart, rangeEnd);
-        //print the array now that it has been filled
+        this.initialArray = new int[rangeEnd];
+        initialArray = getInitialArray(this.rangeStart, this.rangeEnd);
         printThisArray(initialArray);
 
     }
-    //method to fill the initial array based on given ranges
-    public int[] getInitialArray(int rangeStart, int rangeEnd){
+
+    /**
+     * method to fill the initial array based on given ranges
+     * @param rangeStart
+     * @param rangeEnd
+     * @return
+     */
+    private int[] getInitialArray(int rangeStart, int rangeEnd){
         int[] initialArray = new int[rangeEnd+1];
         //fill the initial array based on given range values
         for (int i = rangeStart; i <= rangeEnd; i++){
@@ -45,18 +71,42 @@ public class Question12 {
         return initialArray;
     }
 
-    //method to print the array
-    public void printThisArray(int[] initialArray){
+    /**
+     * method to print the array
+     * @param initialArray
+     */
+    private void printThisArray(int[] initialArray){
         //using enhanced for loop
         for(int i : initialArray){
             if (i%2 == 0 && i != 0){ //make sure we do not print zero!
                 System.out.println(i);
-
             }
-
         }
-
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Question12)) return false;
+        Question12 that = (Question12) o;
+        return Objects.equals(rangeStart, that.rangeStart) &&
+                Objects.equals(rangeEnd, that.rangeEnd) &&
+                Arrays.equals(initialArray, that.initialArray);
+    }
 
+    @Override
+    public int hashCode() {
+        int result = Objects.hash(rangeStart, rangeEnd);
+        result = 31 * result + Arrays.hashCode(initialArray);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Question12{" +
+                "rangeStart=" + rangeStart +
+                ", rangeEnd=" + rangeEnd +
+                ", initialArray=" + Arrays.toString(initialArray) +
+                '}';
+    }
 }
--- troy_davis_code/java/assignment1/question12/src/test/java/com/revature/question12/Question12Test.java
+++ troy_davis_code/java/assignment1/question12/src/test/java/com/revature/question12/Question12Test.java
@@ -41,14 +41,33 @@ public class Question12Test {
     @Test
     public void notNullTest(){
         msg = "make sure neither array is not null";
-        sut = new Question12();
+        sut = new Question12(null, null);
+        expectedReturn = "";
+        Assert.assertEquals(msg, expectedReturn,outputStream.toString().trim());
     }
 
     @Test
-    public void testForNumberOfElements(){
-        String msg = "When given an array, does the even only array have the correct number or elements";
-        //int number = sut.getEvenOnlyArray().length;
-        //Assert.assertEquals(50, number);
+    public void knownElementsTest(){
+        msg = "when initialized using no args constructor, the method should pring the known values from 2 throgh 100";
+        expectedReturn = "2\r\n4\r\n6\r\n8\r\n10\r\n" +
+                         "12\r\n14\r\n16\r\n18\r\n20\r\n" +
+                         "22\r\n24\r\n26\r\n28\r\n30\r\n" +
+                         "32\r\n34\r\n36\r\n38\r\n40\r\n" +
+                         "42\r\n44\r\n46\r\n48\r\n50\r\n" +
+                         "52\r\n54\r\n56\r\n58\r\n60\r\n" +
+                         "62\r\n64\r\n66\r\n68\r\n70\r\n" +
+                         "72\r\n74\r\n76\r\n78\r\n80\r\n" +
+                         "82\r\n84\r\n86\r\n88\r\n90\r\n" +
+                         "92\r\n94\r\n96\r\n98\r\n100";
+        sut = new Question12();
+        Assert.assertEquals(msg, expectedReturn, outputStream.toString().trim());
+    }
+    @Test
+    public void outOfRangeInputsTest(){
+        msg = "When given a start value that is higher than the end value, the method should print nothing";
+        expectedReturn = "";
+        sut = new Question12(1000, 1);
+        Assert.assertEquals(msg, expectedReturn, outputStream.toString().trim());
     }
 
 }
