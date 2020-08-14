2592817a76c0fd2954cf48577f8d4838abc53996
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-08-14 15:26:31 -0400

    finished question 8, 3 test cases

--- troy_davis_code/java/assignment1/question8/src/main/java/com/revature/question8/Question8.java
+++ troy_davis_code/java/assignment1/question8/src/main/java/com/revature/question8/Question8.java
@@ -1,25 +1,85 @@
-/*
-    Q8. Write a program that stores the following strings in an ArrayList and saves all the palindromes in another ArrayList.
-
-“karan”, “madam”, ”tom”, “civic”, “radar”, “jimmy”, “kayak”, “john”,  “refer”, “billy”, “did”
+/**
+ * Q8. Write a program that stores the following strings in an ArrayList and saves all the palindromes in another ArrayList.
+ *
+ * “karan”, “madam”, ”tom”, “civic”, “radar”, “jimmy”, “kayak”, “john”,  “refer”, “billy”, “did”
  */
 package com.revature.question8;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class Question8 {
     private ArrayList<String> storageList;
     private ArrayList<String> palindromeList;
 
+    //variable argument constructor to evaluate strigs for palindromes upon instantiation
     public Question8(String... someStrings){
+        //initialize the arrays
+        storageList = new ArrayList<>();
+        palindromeList = new ArrayList<>();
+        //check for null values
+        if (someStrings != null){
+            //for each of teh strings provided,
+            for (String s : someStrings){
+                //add to storage list,
+                storageList.add(s);
+                //then check to see if it is a palindrome
+                if(isThisAPalindrome(s)){
+                    //if it is, add it to the list
+                    palindromeList.add(s);
+                }
+            }
+        }
+
 
     }
 
-
-
     private boolean isThisAPalindrome(String someString){
-        boolean bool = false;
+        //create new string builder based off of the given string
+        StringBuilder reversed = new StringBuilder (someString);
+        //create a return string to hold the reversed string
+        String r = reversed.reverse().toString();
+        //if the string is the same as itself reversed, it is a palindrome, return it
+        return r.equals(someString);
 
-        return bool;
+
+    }
+
+    public ArrayList<String> getStorageList() {
+        return storageList;
+    }
+
+    public void setStorageList(ArrayList<String> storageList) {
+        this.storageList = storageList;
+    }
+
+    public ArrayList<String> getPalindromeList() {
+        return palindromeList;
+    }
+
+    public void setPalindromeList(ArrayList<String> palindromeList) {
+        this.palindromeList = palindromeList;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Question8)) return false;
+        Question8 question8 = (Question8) o;
+        return Objects.equals(getStorageList(), question8.getStorageList()) &&
+                Objects.equals(getPalindromeList(), question8.getPalindromeList());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getStorageList(), getPalindromeList());
+    }
+
+    @Override
+    public String toString() {
+        return "Question8{" +
+                "storageList=" + storageList +
+                ", palindromeList=" + palindromeList +
+                '}';
     }
 }
--- troy_davis_code/java/assignment1/question8/src/test/java/com/revature/question8Test/Question8Test.java
+++ troy_davis_code/java/assignment1/question8/src/test/java/com/revature/question8Test/Question8Test.java
@@ -1,7 +1,67 @@
+/**
+ * Q8. Write a program that stores the following strings in an ArrayList and saves all the palindromes in another ArrayList.
+ *
+ * “karan”, “madam”, ”tom”, “civic”, “radar”, “jimmy”, “kayak”, “john”,  “refer”, “billy”, “did”
+ */
 package com.revature.question8Test;
 
 import com.revature.question8.Question8;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
 
 public class Question8Test {
-    //Question8 q = new Question8();
+    Question8 sut;
+    String msg;
+    ArrayList<String> expectedResult;
+
+    @After
+    public void tearDown(){
+        sut = null;
+        msg = null;
+        expectedResult = null;
+    }
+
+    @Test
+    public void allStringsInArrayListTest(){
+        msg = "When given all the strings from the task, the method should store them all in the appropriate array list.";
+        expectedResult = new ArrayList<>();
+        expectedResult.add("karen");
+        expectedResult.add("madam");
+        expectedResult.add("tom");
+        expectedResult.add("civic");
+        expectedResult.add("radar");
+        expectedResult.add("jimmy");
+        expectedResult.add("kayak");
+        expectedResult.add("john");
+        expectedResult.add("refer");
+        expectedResult.add("billy");
+        expectedResult.add("did");
+        sut = new Question8("karen","madam", "tom", "civic", "radar","jimmy","kayak", "john", "refer", "billy", "did");
+        Assert.assertEquals(msg, expectedResult, sut.getStorageList());
+    }
+    @Test
+    public void palindromesOnlyTest(){
+        msg = "when given the full list of strings, the class should store only the palindromes in the correct arrayList";
+        expectedResult = new ArrayList<>();
+        expectedResult.add("madam");
+        expectedResult.add("civic");
+        expectedResult.add("radar");
+        expectedResult.add("kayak");
+        expectedResult.add("refer");
+        expectedResult.add("did");
+        sut = new Question8("karen","madam", "tom", "civic", "radar","jimmy","kayak", "john", "refer", "billy", "did");
+        Assert.assertEquals(msg, expectedResult, sut.getPalindromeList());
+    }
+    @Test
+    public void nullStringsTest(){
+        msg = "when given null strings, the class store store an enpty ArrayList for the storage and palindrome list";
+        expectedResult = new ArrayList<>();
+        sut = new Question8(null);
+        Assert.assertEquals(msg,expectedResult, sut.getPalindromeList());
+        Assert.assertEquals(msg, expectedResult, sut.getStorageList());
+    }
+
 }
