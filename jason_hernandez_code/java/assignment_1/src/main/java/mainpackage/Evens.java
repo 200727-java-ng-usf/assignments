1e6d20a6e14e4b0e328295e63c7e04332c3f7493
jason-hernandez-73 <56489096+jason-hernandez-73@users.noreply.github.com>
2020-08-15 12:48:03 -0700

    Added another array to Evens per instructions

--- jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/Evens.java
+++ jason_hernandez_code/java/assignment_1/src/main/java/mainpackage/Evens.java
@@ -4,26 +4,20 @@ import java.util.ArrayList;
 
 public class Evens {
     public void getEvens() {
-        // Initialize two arrays
+        // Initialize array
         ArrayList<Integer> numbers2 = new ArrayList<Integer>(100);
         ArrayList<Integer> evenNumbers = new ArrayList<Integer>(50);
 
         // Populate the first array with integers 1 to 100
         for (int i = 1; i <= 100; i++) {
-//            numbers2.add(i);
-//
-//            // Check each number to see if it is even
-//            int temp;
-//            boolean isEven = false;
-//
-//            for (int j = 1; j <= i; j++) {
-//                temp = i % 2;
+            numbers2.add(i);
+
+
+           // Check each number to see if it is even
+            for (int j = 1; j <= numbers2.size(); j++)
                 if (i%2 == 0) {
-//                    isEven = true;
-//                    break;
-//                }
-//                //If isEven is true then the number is even else not
-//                if (isEven)
+                    // If so add it to the second array
+                    if(!evenNumbers.contains(i))
                     evenNumbers.add(i);
 
             }
