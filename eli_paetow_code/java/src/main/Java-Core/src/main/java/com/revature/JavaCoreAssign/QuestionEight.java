5de59af19470839d4fa59d9fcd3691141f401705
EPaetow <elipaetow@yahoo.com>
2020-08-14 20:24:28 -0500

    come back to q8

--- eli_paetow_code/java/src/main/Java-Core/src/main/java/com/revature/JavaCoreAssign/QuestionEight.java
+++ eli_paetow_code/java/src/main/Java-Core/src/main/java/com/revature/JavaCoreAssign/QuestionEight.java
@@ -1,4 +1,65 @@
 package com.revature.JavaCoreAssign;
 
+import java.util.ArrayList;
+
+//Q8. Write a program that stores the following strings in an ArrayList and saves
+// all the palindromes in another ArrayList.
+//
+//“karan”, “madam”, ”tom”, “civic”, “radar”, “jimmy”, “kayak”, “john”,  “refer”, “billy”, “did”
 public class QuestionEight {
+
+    public static ArrayList<String> palindromeQ8(){
+
+        ArrayList<String> allStrings = new ArrayList<String>();
+        allStrings.add("karen");
+        allStrings.add("madam");
+        allStrings.add("tom");
+        allStrings.add("civic");
+        allStrings.add("radar");
+        allStrings.add("jimmy");
+        allStrings.add("kayak");
+        allStrings.add("john");
+        allStrings.add("refer");
+        allStrings.add("billy");
+        allStrings.add("did");
+
+
+        ArrayList<String> palindromesOnly = new ArrayList<String>();
+        ArrayList<String> trashStrings = new ArrayList<String>();
+
+        for (String str: allStrings){
+
+            boolean yesPalindrome = palindromeCheck(str);
+
+            if (yesPalindrome){
+                palindromesOnly.add(str);
+            }else {
+                trashStrings.add(str);
+            }
+
+
+        }
+        System.out.println(allStrings);
+        System.out.println("----------------------");
+        System.out.println("palindromes only!!");
+        System.out.println(palindromesOnly);
+
+        return palindromesOnly;
+    }
+    public static boolean palindromeCheck(String str){
+        if (str.isEmpty()) {
+            return false;
+        }else {
+
+            return new StringBuilder(str).reverse().toString().equals(str);
+        }
+
+
+    }
+
+//    public static void main(String[] args) {
+//        palindromeQ8();
+//
+//    }
+
 }
--- eli_paetow_code/java/src/main/Java-Core/src/test/java/com/revature/JavaCoreAssign/QuestionEightTest.java
+++ eli_paetow_code/java/src/main/Java-Core/src/test/java/com/revature/JavaCoreAssign/QuestionEightTest.java
@@ -1,4 +1,36 @@
 package com.revature.JavaCoreAssign;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 public class QuestionEightTest {
+
+    private QuestionEight sut;
+
+    @Before
+    public void setup() {
+        sut = new QuestionEight();
+    }
+
+    @Test
+    public void palindromeQ8Good() {
+
+        String expectedResult = "[madam, civic, radar, kayak, refer, did]";
+        ArrayList<String> actualResult = sut.palindromeQ8();
+        assertEquals(expectedResult, actualResult);
+
+
+    }
+
+    @After
+    public void tearDown() {
+        sut = null;
+    }
 }
+
+
