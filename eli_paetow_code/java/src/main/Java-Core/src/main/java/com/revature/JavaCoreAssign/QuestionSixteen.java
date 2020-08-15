fe01e64087878c3e2618888f671c05df32c31c15
EPaetow <elipaetow@yahoo.com>
2020-08-14 23:51:53 -0500

    cant figure out testing

--- eli_paetow_code/java/src/main/Java-Core/src/main/java/com/revature/JavaCoreAssign/QuestionSixteen.java
+++ eli_paetow_code/java/src/main/Java-Core/src/main/java/com/revature/JavaCoreAssign/QuestionSixteen.java
@@ -1,4 +1,21 @@
 package com.revature.JavaCoreAssign;
 
+import java.util.Scanner;
+
+//Write a program to display the number of characters for a string input.
+// The string should be entered as a command line argument using (String [ ] args).
+
 public class QuestionSixteen {
+
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("enter a string please: ");
+        String Q16 = input.nextLine();
+
+        for (int i = 0; i <= Q16.length(); i++){
+            i++;
+        }
+        System.out.println("character count is: " + Q16.length());
+    }
 }
--- eli_paetow_code/java/src/main/Java-Core/src/test/java/com/revature/JavaCoreAssign/QuestionSixteenTest.java
+++ eli_paetow_code/java/src/main/Java-Core/src/test/java/com/revature/JavaCoreAssign/QuestionSixteenTest.java
@@ -1,4 +1,40 @@
 package com.revature.JavaCoreAssign;
 
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Scanner;
+
+import static org.junit.Assert.assertEquals;
+
+//Write a program to display the number of characters for a string input.
+// The string should be entered as a command line argument using (String [ ] args).
 public class QuestionSixteenTest {
+
+//    private QuestionSixteen sut;
+//
+//    @Before
+//    public void setup() {
+//        sut = new QuestionSixteen();
+//
+//    }
+//
+//    @Test
+//    public void charCountGood() {
+//        int expectedResult = 5;
+//        int actualResult = sut.charCount("hell0");
+//
+//        assertEquals(expectedResult, actualResult);
+//
+//
+//    }
+//
+//    @After
+//    public void tearDown() {
+//        sut = null;
+//    }
+
+
 }
