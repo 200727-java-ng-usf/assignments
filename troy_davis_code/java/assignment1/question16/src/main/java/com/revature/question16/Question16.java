11c91e0771069e94982b3d3d49c12ee55e2386a8
tmd1990us <68717655+tmd1990us@users.noreply.github.com>
2020-08-14 21:31:13 -0400

    finished question 16, came up with 3 test cases

--- troy_davis_code/java/assignment1/question16/src/main/java/com/revature/question16/Question16.java
+++ troy_davis_code/java/assignment1/question16/src/main/java/com/revature/question16/Question16.java
@@ -5,21 +5,63 @@
 
 package com.revature.question16;
 
-import java.util.Scanner;
+import java.util.Objects;
 
 public class Question16 {
     private int numberOfCharacters;
 
-    public Question16() {
-        this.numberOfCharacters = numberOfCharacters;
-        Scanner scanner = new Scanner(System.in);
-        //numberOfCharacters = getNumberOfCharacters(scanner.next());
+    public Question16(String[] args) {
+        //check for null values first
+        if (args == null){
+            numberOfCharacters = 0;
+        } else {
+            numberOfCharacters = getNumberOfCharactersInThisStringArray(args);
+        }
     }
-    public int getNumberOfCharacters(String[] aString){
 
-        //remove all white spaces
-        String[] temp = aString;
+    //private method to find the number of chars in a given array of strings
+    private int getNumberOfCharactersInThisStringArray(String[] aString){
+        int tempCharCounter = 0;
+        //iterate through the array, counting the chars one by one
+        for (String s : aString){
+            s = s.trim();
+            //iterate through each string, ignoring white spaces
+            for (int i = 0; i < s.length(); i++){
+                if (s.charAt(i) != ' '){
+                    tempCharCounter++;
+                }
+            }
 
-        return 0;
+        }
+
+        return tempCharCounter;
+    }
+
+    public int getNumberOfCharacters() {
+        return numberOfCharacters;
+    }
+
+    public void setNumberOfCharacters(int numberOfCharacters) {
+        this.numberOfCharacters = numberOfCharacters;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Question16)) return false;
+        Question16 that = (Question16) o;
+        return getNumberOfCharacters() == that.getNumberOfCharacters();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getNumberOfCharacters());
+    }
+
+    @Override
+    public String toString() {
+        return "Question16{" +
+                "numberOfCharacters=" + numberOfCharacters +
+                '}';
     }
 }
--- troy_davis_code/java/assignment1/question16/src/test/java/com/revature/question16Tests/Question16Tests.java
+++ troy_davis_code/java/assignment1/question16/src/test/java/com/revature/question16Tests/Question16Tests.java
@@ -6,31 +6,46 @@ package com.revature.question16Tests;
 
 import com.revature.question16.Question16;
 import org.junit.After;
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
-
-import java.io.BufferedWriter;
 
 public class Question16Tests {
     Question16 sut;
-    int characterCount;
+    int expectedReturn;
+    String msg;
+    String[] input;
 
-    @Before
-    public void setup(){
-        sut = new Question16();
-        characterCount = 0;
-    }
 
     @After
     public void tearDown(){
         sut = null;
-
+        expectedReturn = 0;
+        msg = null;
     }
 
     @Test
     public void testWhiteSpaces(){
-        String msg = "when given a string with white spaces, does the class remove them?";
-        String stringWithSpaces = "he llo w rold ";
+        msg = "when given a string with white spaces, does the class remove them?";
+        input = new String[]{"ma r k   ", "jo h n "};
+        sut = new Question16(input);
+        expectedReturn = 8;
+        Assert.assertEquals(msg, expectedReturn,sut.getNumberOfCharacters());
+    }
 
+    @Test
+    public void nullInputTest(){
+        msg = "when given null input, the class should store 0 as the number of characters";
+        input = null;
+        sut = new Question16(input);
+        expectedReturn = 0;
+        Assert.assertEquals(msg,expectedReturn,sut.getNumberOfCharacters());
+    }
+    @Test
+    public void helloWorldTest(){
+        msg = "when given Hello World as input, the method should detect that there are 10 characters";
+        input = new String[]{"Hello", "World"};
+        sut = new Question16(input);
+        expectedReturn = 10;
+        Assert.assertEquals(msg,expectedReturn,sut.getNumberOfCharacters());
     }
 }
