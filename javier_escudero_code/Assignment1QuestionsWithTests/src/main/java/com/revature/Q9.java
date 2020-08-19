65f077e022ac53b7f07946d74e11f0210b66faf0
escuderj <javierusn@yahoo.com>
2020-08-18 20:37:04 -0400

    Q9 and Tests

--- javier_escudero_code/Assignment1QuestionsWithTests/src/main/java/com/revature/Q9.java
+++ javier_escudero_code/Assignment1QuestionsWithTests/src/main/java/com/revature/Q9.java
@@ -7,58 +7,61 @@ public class Q9 {
      * Q9. Create an ArrayList which stores numbers from 1 to
      * 100 and prints out all the prime numbers to the console.
      */
-    static ArrayList<Integer> arrayList = new ArrayList<>();
-    static ArrayList<Integer> primeList = new ArrayList<>();
-    static public void storeNumbers1to100(){
+     ArrayList<Integer> arrayList = new ArrayList<>();
+     ArrayList<Integer> primeList = new ArrayList<>();
+
+    public void storeNumbers1to100(){
 
         // stores numbers 1 to 100
         for(int i = 0; i < 100; i++){
             arrayList.add(i+1);
         }
-        //prime number is > 1 and only divisible by 1 and itself
-        System.out.println("\narrayList " +arrayList);
     }
 
-    static public void /*ArrayList<Integer> */ onlyPrime1to100(){
+    static public void onlyPrime1to100(){
+
+        Q9 q9 = new Q9();
+        q9.storeNumbers1to100();
 
         // copy arraylist to primeList
-        for(int i = 0; i < 100; i++) {
-            primeList.add(arrayList.get(i));
+        for(int i = 1; i < 100; i++) {
+            q9.primeList.add(q9.arrayList.get(i));
         }
 
         // delete composite numbers (not prime) from the list
-        primeList.remove(0); // deleting 1
+     //   primeList.contains(primeList.indexOf()); // deleting 1
+
+   //     primeList.remove(0);
 
 //        primeList.remove(98);
         int i = 0;
         int divisor = 1;
         int factorCount = 0;
-        System.out.println(arrayList.size());
-        while (primeList.get(i) < (arrayList.size() )) {// 100 !=  100 while last number is not the last
-            while( ( ( primeList.get(i) % divisor ) == 0 ) && ( divisor <= primeList.get(i) ) ) {
+       // System.out.println(arrayList.size());
+        while (q9.primeList.get(i) < (q9.arrayList.size() )) {// 100 !=  100 while last number is not the last
+            while( ( ( q9.primeList.get(i) % divisor ) == 0 ) && ( divisor <= q9.primeList.get(i) ) ) {
                 ++factorCount;//2:1 2  3:1 2      4: 1 2  5: 1 2   6:1 2 3
                 ++divisor;    //2:2 3  3:2 4      4: 2 5  5: 2 61  6:2 3 4
-                if( factorCount >= 3 || divisor > (primeList.get(i) +1 ) ){ // 6 count ==3
-                    primeList.remove(i); // 4 removed, 5, 6
+                if( factorCount >= 3 || divisor > (q9.primeList.get(i) +1 ) ){ // 6 count ==3
+                    q9.primeList.remove(i); // 4 removed, 5, 6
                     divisor = 1;
                     factorCount = 0;
                 }
             }
-            if ( (primeList.get(i) % divisor != 0)) {
+            if ( (q9.primeList.get(i) % divisor != 0)) {
                 ++divisor;
             }
-            if( divisor > primeList.get(i)){
+            if( divisor > q9.primeList.get(i)){
                 ++i;// 1
                 divisor = 1;
                 factorCount = 0;
             }
         }
-        primeList.remove((primeList.size()-1)); // removes the last index, i.e. 100
+        q9.primeList.remove((q9.primeList.size()-1)); // removes the last index, i.e. 100
 }
 
-    public static void main(String[] args) {
-        Q9.storeNumbers1to100();
-        Q9.onlyPrime1to100();
-        System.out.println("Prime" + Q9.primeList);
-    }
+//    public static void main(String[] args) {
+//        Q9.onlyPrime1to100();
+//        System.out.println("Prime" + Q9.primeList);
+//    }
 }
--- javier_escudero_code/Assignment1QuestionsWithTests/src/test/java/com/revature/Q9Test.java
+++ javier_escudero_code/Assignment1QuestionsWithTests/src/test/java/com/revature/Q9Test.java
@@ -1,7 +1,39 @@
 package com.revature;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+
 import static org.junit.Assert.*;
 
 public class Q9Test {
 
+    private Q9 q9;
+
+    @Test
+    public void testIfEvenNumberFound() {
+        q9 = new Q9();
+        Q9.onlyPrime1to100();
+        for (int i = 1; i < q9.primeList.size() ; i++) {
+            assertTrue(" no even numbers found", ((q9.primeList).get(i) % 2 != 0));
+        }
+    }
+
+    @Test
+    public void testIfDivisibleBy3NumberFound() {
+        Q9 q9 = new Q9();
+        Q9.onlyPrime1to100();
+        for (int i = 2; i < q9.primeList.size() - 2; i++) {
+            assertTrue(" no divisible by 3 numbers found", ((q9.primeList).get(i + 1) % 3 != 0));
+        }
+    }
+
+    @Test
+    public void testIfDivisibleBy5NumberFound() {
+        Q9 q9 = new Q9();
+        Q9.onlyPrime1to100();
+        for (int i = 3; i < q9.primeList.size() ; i++) {
+            assertTrue(" no divisible by 5 numbers found", ((q9.primeList).get(i) % 5 != 0));
+        }
+    }
 }
