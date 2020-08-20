ca9a91e02b8e9dad51274070afb6edea1c86f4bf
mcoffma1 <47868221+mcoffma1@users.noreply.github.com>
2020-08-19 23:21:01 -0400

    Final push for the day

--- michael_coffman_code/Homework2/Assignment2.sql
+++ michael_coffman_code/Homework2/Assignment2.sql
@@ -1,10 +1,12 @@
 ---------------------- 2.1 SELECT ---------------------------
 
 -- Task 1:
-select * from "Employee";
+select * 
+from "Employee";
 
 -- Task 2:
-select * from "Employee" 
+select * 
+from "Employee" 
 where "LastName" = 'King';
 
 -- Task 3:
@@ -16,9 +18,9 @@ where "FirstName" = 'Andrew' and "ReportsTo" is null;
 ---------------------- 2.2 ORDER BY ---------------------------
 
 -- Task 1:
-SELECT *
+SELECT "Title"
 FROM "Album"
-ORDER BY "AlbumId" DESC;
+ORDER BY "Title" DESC;
 
 -- Task 2:
 SELECT "FirstName"
@@ -33,7 +35,7 @@ VALUES
 	(26, 'Parody'),
 	(27, 'Remix');
 
-SELECT * FROM "Genre";
+
 
 -- Task 2:
 INSERT INTO "Employee" ("EmployeeId", "LastName", "FirstName", "Title", "ReportsTo", "BirthDate", "HireDate", "Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email")
@@ -41,7 +43,6 @@ VALUES
 	(9, 'Coffman', 'Michael', 'Junior Dev', 6, '1991-04-01', '2020-07-27', '1540 Graduation Lane', 'Middleburg', 'FL', 'Merica', '32068', '904-451-0656', '904-444-9239', 'michael.coffman@revature.com'),
 	(10, 'Yankevich', 'Al', 'Musician', 4, '1968-10-17', '1992-05-11', '123 Someplace Dr.', 'Silicon Valley', 'CA', 'Merica', '12345', '987-654-3210', '123-555-7780', 'itsWeirdAlYall@parody.net');
 	
-SELECT * FROM "Employee";
 
 -- Task 3:
 INSERT INTO "Customer" ("CustomerId", "FirstName", "LastName", "Company", "Address", "City", "State", "Country", "PostalCode", "Phone", "Fax", "Email", "SupportRepId") 
@@ -50,7 +51,7 @@ VALUES (60, 'Steve', 'Cart', 'Mining INC', '1010 Ender Isle', 'Pearl Cove', 'Shu
 INSERT INTO "Customer" ("CustomerId", "FirstName", "LastName", "Address", "City", "Country", "PostalCode", "Phone", "Email", "SupportRepId") 
 VALUES (61, 'Dory', 'The Fish', '42 Wallaby Way', 'Sydney', 'Australia', '42042', '111-222-333', 'gottaFindNemo@dis.com', 2);
 
-SELECT * FROM "Customer";
+
 
 
 -------------------- 2.4 UPDATE ---------------------------
@@ -63,11 +64,116 @@ SET
 WHERE 
 	"CustomerId" = 32;
 
-SELECT * 
-FROM "Customer"
-ORDER BY "CustomerId";
 
 -- Task 2:
+UPDATE "Artist" 
+SET
+	"Name" = 'CCR'
+WHERE 
+	"ArtistId" = 76;
+
+-------------------- 2.5 LIKE ---------------------------
+
+-- Task 1:
+SELECT "BillingAddress" 
+FROM "Invoice"
+WHERE "BillingAddress" 
+	LIKE 'T%';
+
+-------------------- 2.6 BETWEEN ------------------------
+
+-- Task 1:
+SELECT "Total"
+FROM "Invoice"
+WHERE "Total" 
+	BETWEEN 15 AND 20;
+
+-- Task 2:
+SELECT *
+FROM "Employee"
+WHERE "HireDate" 
+	BETWEEN '2003-06-01' AND '2004-03-01';
+
+
+-------------------- 2.7 DELETE ------------------------
+
+-- Task 1:
+
+/*
+ * 	We must first address the first error. This is to add 'on delete cascade'
+ * 	to the foreign key of table "Invoice"; this references the customer.
+ * 	To change data such as this, it must first be dropped and then re-added
+ * 	with the correct constraint arguments.
+ */
+
+ALTER TABLE "Invoice"
+DROP CONSTRAINT "FK_InvoiceCustomerId";
+
+ALTER TABLE "Invoice" 
+ADD CONSTRAINT "FK_InvoiceCustomerId"
+  FOREIGN KEY ("CustomerId") 
+  REFERENCES "Customer" ("CustomerId") 
+  ON DELETE CASCADE ON UPDATE NO ACTION;
+  
+/*
+ * 	After the above command, the IDE tells us that there is still a reference.
+ * 	This is a junction table referencing back to the invoice. Adding the delete cascade
+ * 	keyword here essentially allows us to delete a customer, and all invoices we have 
+ * 	of said customer
+ */
+ 
+ALTER TABLE "InvoiceLine"
+DROP CONSTRAINT "FK_InvoiceLineInvoiceId";
+
+ALTER TABLE "InvoiceLine" 
+ADD CONSTRAINT "FK_InvoiceLineInvoiceId"
+  FOREIGN KEY ("InvoiceId") 
+  REFERENCES "Invoice" ("InvoiceId") 
+  ON DELETE CASCADE ON UPDATE NO ACTION;
+ 
+/*
+ * 	Then we're allowed to finally delete any customer from "Customer" and all
+ * 	referencing tables will delete their relative record.
+ * 	..... this question took too long lol
+ */
+
+DELETE FROM "Customer"  
+WHERE
+	"FirstName" = 'Robert'
+	AND 
+	"LastName" = 'Walter';
+
+
+--------------- 3.1 System Defined Functions---------------
+
+-- Task 1:
+SELECT to_char(clock_timestamp() , 'HH12 : MI : SS PM') AS current_time;
+
+-- Task 2:
+SELECT "Name", length("Name") AS MediaType_Length
+FROM "MediaType"
+WHERE "MediaTypeId" = 2;
+-- displays length of 'Protected AAC audio file' = 24
+
+
+---------- 3.2 System Defined Aggregate Functions-----------
+
+-- Task 1:
+SELECT count("Total") AS num_of_invoices, avg("Total")::numeric(5, 2) AS average_total
+FROM "Invoice";
+
+-- Task 2:
+SELECT "Name", "UnitPrice"
+FROM "Track"
+WHERE "UnitPrice" 
+	= (SELECT max("UnitPrice") FROM "Track");
+		-- this was original statement, but I wanted to show
+		-- the name next to it, and realized it only showed 
+		-- 1 record of 1.99 instead of 213... all good now
+
+
+
+
 
 
 
