3983af2833eab05b52a1a1fcc5eeb5935a00480e
YuanfnegGu <yuanfenggu@hotmail.com>
2020-08-20 20:24:23 -0500

    Assignment Completed

--- Assignment2/Assignment2.sql
+++ Assignment2/Assignment2.sql
@@ -93,10 +93,88 @@
 --  (There may be constraints that rely on this, find out how to resolve them).
 
       
-DELETE FROM "Assignment_2_chinook"."InvoiceLine" WHERE "InvoiceId" IN 
-     (SELECT "InvoiceId" FROM "Assignment_2_chinook"."Invoice" WHERE "CustomerId" = 
-        (SELECT "CustomerId" FROM "Assignment_2_chinook"."Customer" WHERE  "FirstName" = 'Robert' AND "LastName" = 'Walter'));
-DELETE FROM "Assignment_2_chinook"."Invoice" WHERE "CustomerId" = (SELECT "CustomerId" FROM "Assignment_2_chinook"."Customer"  WHERE "FirstName" = 'Robert' AND "LastName" = 'Walter');
-DELETE FROM "Assignment_2_chinook"."Customer"  WHERE "FirstName" = 'Robert' AND "LastName" = 'Walter';     
+	
+	DELETE FROM "Assignment_2_chinook"."InvoiceLine" i WHERE "InvoiceId" = (SELECT i."InvoiceId" FROM "Assignment_2_chinook"."Invoice" WHERE "CustomerId" = 
+    (select c."CustomerId"  FROM "Assignment_2_chinook"."Customer" c WHERE "LastName" = 'Walter' AND "FirstName" = 'Robert'));
 
+
+
+
+-- 3.0 SQL Functions
+-- In this section you will be using the PostGreSQL system functions, as well as your own functions, to perform various actions against the database
+-- 3.1 System Defined Functions
+
+--  Task – Create a function that returns the current time.
+
+		SELECT CURRENT_TIMESTAMP;
+
+
+--  Task – create a function that returns the length of a mediatype from the mediatype table
+
+		SELECT LENGTH("Name") from "Assignment_2_chinook"."MediaType";
+
+
+-- 3.2 System Defined Aggregate Functions
+-- Task –Create a function that returns the average total of all invoices
+
+	select avg("Total") from "Assignment_2_chinook"."Invoice" 
+
+-- Task – Create a function that returns the most expensive track
+
+	select * from "Assignment_2_chinook"."Track" t ;
+
+	select max("UnitPrice" ) from "Assignment_2_chinook"."Track" ;
+	
+	
+-- 3.3 User Defined Scalar Functions
+-- Task – Create a function that returns the average price of invoice-line items in the invoice-line table
+    select avg("UnitPrice" ) from "Assignment_2_chinook"."InvoiceLine" il ;
+	
+-- 3.4 User Defined Table Valued Functions
+-- Task – Create a function that returns all employees who are born after 1968.
+
+   select * from "Assignment_2_chinook"."Employee" e 
+   where "BirthDate">'31-DEC-1968';
+  
+--  5.0 JOINS
+--  In this section you will be working with combining various tables through the use of joins. You will work with outer, inner, right, left, cross, and self joins.
+--  5.1 INNER
+--  Task – Create an inner join that joins customers and orders and specifies the name of the customer and the invoiceId.
+	
+  select c."FirstName" ,c."LastName", i."InvoiceId" 
+  from "Assignment_2_chinook"."Customer" c
+  inner join "Assignment_2_chinook"."Invoice" i on (c."CustomerId" =i."CustomerId" );
+  
+
+  
+  
+-- 5.2 OUTER
+--  Task – Create an outer join that joins the customer and invoice table, specifying the CustomerId, firstname, last name, invoiceId, and total.
+	
+	 select c2."CustomerId" ,c2."FirstName" ,c2."LastName" ,i2."InvoiceId" ,i2."Total" 
+	 from "Assignment_2_chinook"."Customer" c2 
+	 full outer join "Assignment_2_chinook"."Invoice" i2 on(c2."CustomerId" =i2."CustomerId" )
+ 
+
+ 
+-- 5.3 RIGHT
+-- Task – Create a right join that joins album and artist specifying artist name and title.
+   select a3."Name" ,a2."Title" 
+   from "Assignment_2_chinook"."Album" a2 
+   right join "Assignment_2_chinook"."Artist" a3 on(a2."ArtistId" =a3."ArtistId" );
+	 
+	 
+-- 5.4 CROSS
+-- Task – Create a cross join that joins album and artist and sorts by artist name in ascending order.
+
+  	select * from "Assignment_2_chinook"."Album" a2 
+  	cross join "Assignment_2_chinook"."Artist" a3 
+  	order by a3."Name" asc ;
+
+
+--  5.5 SELF
+--  Task – Perform a self-join on the employee table, joining on the reports to column.
+
+  select * from "Assignment_2_chinook"."Employee" e 
+  left join "Assignment_2_chinook"."Employee" "manager" on(e."ReportsTo" ="manager"."EmployeeId" );
 	
