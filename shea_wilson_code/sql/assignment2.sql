360c72866b2b32296838bf6ad27c0a95e97b7c93
Shea Wilson <sheawilsn@gmail.com>
2020-08-23 10:07:10 -0500

    Still need to work on joins

--- shea_wilson_code/sql/assignment2.sql
+++ shea_wilson_code/sql/assignment2.sql
@@ -47,7 +47,10 @@ values
 
 --Task – Insert two new records into Customer table
 select * from "Customer";
-insert into "Customer" ("CustomerId","FirstName" ,"LastName","Company" ,"Address" ,"City" )
+insert into "Customer" ("CustomerId","FirstName" ,"LastName","Company" ,"Address" ,"City" ,"State" , "Country" ,"PostalCode","Phone" ,"Fax" ,"Email" ,"SupportRepId" )
+values 
+	(62,'Shea', 'Wilson',null,'11202 Willowood Dr','Corpus Christi', 'TX','USA','78410','6198502811',null,'sheawilsn@revature.com',3),
+	(63,'Martha', 'Wilson',null,'11202 Willowood Dr','Corpus Christi', 'TX','USA','78410','6198502811',null,'mwilson@gmail.com',3);
 
 
 --2.4 UPDATE
@@ -76,23 +79,118 @@ where "BillingAddress" like '%T%';
 select * from "Invoice"
 where "Total" between 15 and 50;
 
-
 -- Task – Select all employees hired between 1st of June 2003 and 1st of March 2004
 select * from "Employee"
 where "HireDate" between timestamp'2003-06-01' and timestamp'2004-03-01';
 
 --2.7 DELETE
 --Task – Delete a record in Customer table where the name is Robert Walter (There may be constraints that rely on this, find out how to resolve them)
-select * from "InvoiceLine" 
-alter table "Invoice" 
-add foreign key (InvoiceId_Fk)
-references "InvoiceLine"(InvoiceId)
-on delete cascade ;
-
 select * from "Customer";
-delete from "Customer" 
-where "CustomerId" = 32;
 
+--3.1 System Defined Functions
+
+--Task – Create a function that returns the current time.
+
+create or replace function return_current_time()
+returns time
+as'
+begin
+      
+return current_time ;
+
+end
+'
+language plpgsql;
+
+select return_current_time();
+
+--Task – create a function that returns the length of a mediatype from the mediatype table
+
+select * from "MediaType" mt;
+
+create or replace function length_of_mediatype (e numeric )
+returns numeric 
+as $$
+	
+	declare 
+	mediatype_length integer ;
+	mediatype_name text;
+	id integer;
+
+	begin 
+
+		select ("MediaTypeId")
+		into id
+		from "MediaType";
+		
+		select ("Name")
+		into mediatype_name
+		from "MediaType";
+			
+		 mediatype_length := length(mediatype_name) ;
+		return mediatype_length;
+	end
+$$
+language plpgsql;
+
+select length_of_mediatype(3);
+
+
+--3.2 System Defined Aggregate Functions
+--Task –Create a function that returns the average total of all invoices
+
+select * from "Invoice" i ;
+select avg("Total")
+from "Invoice" i ;
+
+create or update function avg_total()
+returns int
+as $$
+	declare invoice_avg integer;
+begin
+	select("Total")
+	from "Invoice";
+
+	invoice_avg := avg("Total") ;
+	return invoice_avg;
+end
+$$ language plpgsql;
+
+select avg_total();
+
+
+
+--Task – Create a function that returns the most expensive track
+select * from "Track";
+select max("UnitPrice")
+from "Track" t ;
+
+--3.3 User Defined Scalar Functions
+--Task – Create a function that returns the average price of invoice-line items in the invoice-line table
+
+select * from "InvoiceLine" il ;
+select avg("UnitPrice" )
+from "InvoiceLine" il;
+
+--3.4 User Defined Table Valued Functions
+--Task – Create a function that returns all employees who are born after 1968.
+
+select * from "Employee" e2 
+where ("BirthDate" ) > timestamp '1968-12-31';
+
+
+--5.0 JOINS
+--In this section you will be working with combining various tables through the use of joins. You will work with outer, inner, right, left, cross, and self joins.
+--5.1 INNER
+--Task – Create an inner join that joins customers and orders and specifies the name of the customer and the invoiceId.
+--5.2 OUTER
+--Task – Create an outer join that joins the customer and invoice table, specifying the CustomerId, firstname, last name, invoiceId, and total.
+--5.3 RIGHT
+--Task – Create a right join that joins album and artist specifying artist name and title.
+--5.4 CROSS
+--Task – Create a cross join that joins album and artist and sorts by artist name in ascending order.
+--5.5 SELF
+--Task – Perform a self-join on the employee table, joining on the reports to column.
 
 
 
