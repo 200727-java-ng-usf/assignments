e8685c58df2da46657e10e2806e217bfb2adcbff
escuderj <javierusn@yahoo.com>
2020-08-23 16:00:46 -0400

    Q13 and test

--- javier_escudero_code/Assignment1QuestionsWithTests/src/main/java/com/revature/Q13.java
+++ javier_escudero_code/Assignment1QuestionsWithTests/src/main/java/com/revature/Q13.java
@@ -16,24 +16,31 @@ public class Q13 {
      *  *                                       ...
      */
     static int index = 0;
-
-    public static void main(String[] args) {
+    static public int[] pattern0And1Printer() {
         int printCount = 1, printCountIncrement = 1;
 
-        while ( printCount <= array0And1.length) {
-            for (; index < printCount; index++ )
-                System.out.print( array0And1[index] ); // output: printCount times
+        while (printCount <= array0And1.length) {
+            for (; index < printCount; index++)
+                System.out.print(array0And1[index]); // output: printCount times
             System.out.println(); // next line
             printCountIncrement++;
             printCount = printCount + printCountIncrement;
         }
+        return array0And1;
     }
-}/**
+
+    public static void main(String[] args){
+        int []arr = Q13.pattern0And1Printer();
+        System.out.print(arr);
+    }
+}
+/**
  * static index = 0        < printCount = 1 |
  *                           printCount + printCountIncrement++        |  array0And1[index] output
  *  [0]          index++   <     1                  2                  |       0
  *  [1][2]       index++   <     3                  3                  |       10
- *  [3][4][5]    index++   <     6                  4                  |       010
- *  [6][7][8][9] index++   <     10                 5                  |       1010
+ *  [3][4][5]    index++   <     6                  4                  |       101
+ *  [6][7][8][9] index++   <     10                 5                  |       0101
  *  ...                    <    ...                ...                 |       ...
  */
+
--- javier_escudero_code/Assignment1QuestionsWithTests/src/test/java/com/revature/Q13Test.java
+++ javier_escudero_code/Assignment1QuestionsWithTests/src/test/java/com/revature/Q13Test.java
@@ -1,4 +1,22 @@
-import static org.junit.Assert.*;
+package com.revature;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
 public class Q13Test {
-  
+
+    private Q13 q13;
+
+
+    @Before
+    public void tSetup() {
+        q13 = new Q13();
+    }
+
+    @Test
+    public void test() {
+        int[] x = (Q13.pattern0And1Printer()); // String x = Arrays.toString(Q13.pattern0And1Printer(6));
+        System.out.println(x);
+    }
 }
