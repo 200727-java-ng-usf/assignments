3a88ce5ce2f7222bc781476c46a8e8dd5ecd6402
mcoffma1 <47868221+mcoffma1@users.noreply.github.com>
2020-09-18 09:49:54 -0400

    Angular Tour de Heroes

--- michael_coffman_code/TourOfHeroes/angular-tour-of-heroes/src/app/app.module.ts
+++ michael_coffman_code/TourOfHeroes/angular-tour-of-heroes/src/app/app.module.ts
@@ -19,6 +19,9 @@ import { MessagesComponent } from './messages/messages.component';
     AppRoutingModule,
     HttpClientModule,
 
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
     )
